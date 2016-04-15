9e8e6049b49f309f8c01d033c757d686d6d1fe6b
Laurence Hubert <laurence.hubert@hurence.com>
2016-04-15 20:33:07 +0200

    Added generic classes to build a testing environment.

---
+++ src/test/java/com/hurence/logisland/integration/RulesPublisher.java
@@ -0,0 +1,10 @@
+package com.hurence.logisland.integration;
+
+
+/**
+ * Publisher to publish rules (CEP rules or matching rules)
+ * Created by lhubert on 15/04/16.
+ */
+public class RulesPublisher extends ConfigPublisher {
+
+}
