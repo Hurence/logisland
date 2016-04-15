bc26e74c1ee9e773da92e123a0d93fff5db6d56c
Thomas Bailet <bailet.thomas@gmail.com>
2016-04-15 11:18:33 +0100

    Create OutlierProcessor.java

---
+++ src/main/java/com/hurence/logisland/processor/OutlierProcessor.java
@@ -0,0 +1,20 @@
+package com.hurence.logisland.processor;
+
+import com.hurence.logisland.event.Event;
+import com.hurence.logisland.log.LogParser;
+import com.hurence.logisland.log.LogParserException;
+
+/**
+ */
+public class OutlierProcessor implements EventProcessor {
+
+    /**
+     * take a line of csv and convert it to a NetworkFlow
+     *
+     * @param s
+     * @return
+     */
+    public Collection<Event> process(Collection<Event> events) {
+    
+    }
+}
