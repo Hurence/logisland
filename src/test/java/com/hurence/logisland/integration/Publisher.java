150bee1cafe9aa6c8667cb05eb86ec6e447edce7
Gregoire Seguin-Henry <s.h.gregoire@gmail.com>
2016-04-19 18:39:55 +0200

    renamed KafkaContext to EmbeddedKafkaEnvironment and added comment to publishers to explain that these are function used for test plugins

--- src/test/java/com/hurence/logisland/integration/Publisher.java
+++ src/test/java/com/hurence/logisland/integration/Publisher.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 /**
  * Interface all Publishers of events should comply to in the testing framework.
  * Created by lhubert on 15/04/16.
+ *
+ * Used for plugin tests
  */
 public interface Publisher {
-    public void publish(KafkaContext context, String path, String topic) throws IOException;
+    public void publish(EmbeddedKafkaEnvironment context, String path, String topic) throws IOException;
 }
