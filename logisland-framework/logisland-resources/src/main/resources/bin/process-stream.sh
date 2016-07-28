2e5c22e7f24f97d08f6e2528fe8626540b6e48be
Mathieu Marie <mathieu.marie@hurence.com>
2016-07-28 12:09:28 +0200

    Fixes for Spark in yarn-cluster mode + improvements to process-stream. sh script + changed maven-assembly-plugin version + shading of guava in elastic search

--- logisland-framework/logisland-resources/src/main/resources/bin/process-stream.sh
+++ logisland-framework/logisland-resources/src/main/resources/bin/process-stream.sh
@@ -2,19 +2,102 @@
 
 source $(dirname $0)/launcher.sh
 
-
-
-#--input-topics cisco-log --output-topics cisco-event --log-parser com.hurence.logisland.plugin.cisco.CiscoLogParser
 declare -r app_mainclass="com.hurence.logisland.runner.StreamProcessingRunner"
-#declare java_cmd="$SPARK_HOME/bin/spark-submit --class ${app_mainclass} --master local[2] --jars ${app_classpath} ${lib_dir}/com.hurence.log-island-*.jar  ${@}"
 
-declare java_cmd="$SPARK_HOME/bin/spark-submit \
+MODE="default"
+VERBOSE_OPTIONS=""
+YARN_CLUSTER_OPTIONS=""
+	
+usage() {
+  echo "Usage:"
+  echo
+  echo " `basename $0` --conf <yml-configuguration-file> [--yarn-cluster] [--spark-home <spark-home-directory>]"
+  echo 
+  echo "Options:"
+  echo
+  echo "  --conf <yml-configuguration-file> : provides the configuration file"
+  echo "  --yarn-cluster : flag to inform that the yarn-cluster mode must be used"
+  echo "  --spark-home : sets the SPARK_HOME (defaults to \$SPARK_HOME environment variable)"
+  echo "  --help : displays help"
+}
+
+if [ $# -eq 0 ]
+then
+  usage
+  exit 1
+fi
+
+while [ $# -gt 0 ]
+do
+  key="$1"
+
+  case $key in
+    --conf)
+      CONF_FILE="$2"
+      shift 
+      ;;
+    --yarn-cluster)
+      MODE="yarn-cluster"
+      YARN_CLUSTER_OPTIONS="--master yarn --deploy-mode cluster"
+      ;;
+    --verbose)
+      VERBOSE_OPTIONS="--verbose"
+      ;;
+    --spark-home)
+      SPARK_HOME="$2"
+      shift
+      ;;
+    --help)
+      usage
+      exit 0
+      ;;
+    *)
+      echo "Unsupported option : $key"
+      usage
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+if [ -z "${SPARK_HOME}" ] 
+then
+  echo "Please provide the --spark-home option or set the SPARK_HOME environment variable"
+  usage
+  exit 1
+fi
+
+if [ -z "${CONF_FILE}" ]
+then
+    echo "The configuration file is missing"
+    usage 
+    exit 1
+fi
+
+if [ ! -f ${CONF_FILE} ]
+then
+  echo "The configuration file ${CONF_FILE} does not exist"
+  usage
+  exit 1
+fi
+
+case $MODE in
+  default)
+    app_classpath=`echo ${app_classpath} | sed 's#,/[^,]*/logisland-elasticsearch-shaded-[^,]*.jar,#,#'`
+    ;;
+  yarn-cluster)
+    app_classpath=`echo ${app_classpath} | sed 's#,/[^,]*/logisland-spark-engine-[^,]*.jar,#,#'`
+    app_classpath=`echo ${app_classpath} | sed 's#,/[^,]*/guava-[^,]*.jar,#,#'`
+    app_classpath=`echo ${app_classpath} | sed 's#,/[^,]*/elasticsearch-[^,]*.jar,#,#'`
+    ;;
+esac
+
+declare java_cmd="${SPARK_HOME}/bin/spark-submit ${VERBOSE_OPTIONS} ${YARN_CLUSTER_OPTIONS} \
     --class ${app_mainclass} \
-    --jars ${app_classpath} ${lib_dir}/logisland*.jar \
-    ${@}"
-
-
-
+    --jars ${app_classpath} \
+    --files ${CONF_FILE}#logisland-configuration.yml \
+    ${lib_dir}/logisland-spark-engine*.jar \
+    -conf logisland-configuration.yml"
 
 echo $java_cmd
 exec $java_cmd
