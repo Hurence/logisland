6ff1a1824829d92248b621cb09e48ba4e9cf3707
Thomas Bailet <bailet.thomas@gmail.com>
2016-10-17 21:20:28 +0200

    change spark to 1.6.2

--- logisland-framework/logisland-spark-engine/src/main/scala/com/hurence/logisland/engine/spark/HdfsBurnerEngine.scala
+++ logisland-framework/logisland-spark-engine/src/main/scala/com/hurence/logisland/engine/spark/HdfsBurnerEngine.scala
@@ -8,7 +8,7 @@ import com.hurence.logisland.component.PropertyDescriptor
 import com.hurence.logisland.engine.EngineContext
 import com.hurence.logisland.processor.StandardProcessContext
 import com.hurence.logisland.processor.chain.{KafkaRecordStream, StandardProcessorChainInstance}
-import com.hurence.logisland.record.{Field, FieldDictionary, FieldType, Record}
+import com.hurence.logisland.record.{Field, FieldType, Record}
 import com.hurence.logisland.util.kafka.KafkaSink
 import com.hurence.logisland.util.spark.ZookeeperSink
 import com.hurence.logisland.util.validator.StandardValidators
@@ -96,96 +96,108 @@ class HdfsBurnerEngine extends AbstractSparkStreamProcessingEngine {
                 kafkaSink: Broadcast[KafkaSink],
                 zkSink: Broadcast[ZookeeperSink]): Unit = {
 
+        if (!rdd.isEmpty()) {
+            val processorChainContext = new StandardProcessContext(processorChainInstance)
+            // Cast the rdd to an interface that lets us get an array of OffsetRange
+            val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
 
-        val processorChainContext = new StandardProcessContext(processorChainInstance)
-        // Cast the rdd to an interface that lets us get an array of OffsetRange
-        val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+            // Get the singleton instance of SQLContext
+            val sqlContext = new org.apache.spark.sql.SQLContext(rdd.sparkContext)
+            // this is used to implicitly convert an RDD to a DataFrame.
 
-        // Get the singleton instance of SQLContext
-        val sqlContext = new org.apache.spark.sql.SQLContext(rdd.sparkContext)
-        // this is used to implicitly convert an RDD to a DataFrame.
-
-        val deserializer = getSerializer(
-            processorChainContext.getProperty(KafkaRecordStream.INPUT_SERIALIZER).asString,
-            processorChainContext.getProperty(KafkaRecordStream.AVRO_INPUT_SCHEMA).asString)
-
-
-
-
-        def convertToRow(record: Record): Row = {
-            Row.fromSeq(
-                record.getAllFieldsSorted.toArray(Array[Field]()).map(f => {
-                    f.getType match {
-                        case FieldType.INT => f.asInteger().intValue()
-                        case FieldType.LONG => f.asLong().longValue()
-                        case FieldType.FLOAT => f.asFloat().floatValue()
-                        case FieldType.DOUBLE => f.asDouble().doubleValue()
-                        case FieldType.STRING => f.asString()
-                        case _ => f.asString()
-                    }
-                }).toSeq
-            )
-        }
-
-        def convertFieldsNameToSchema(record: Record): StructType = {
-            StructType(
-                record.getAllFieldsSorted.toArray(Array[Field]()).map(f => {
-                    f.getType match {
-                        case FieldType.INT => StructField(f.getName, IntegerType, nullable = true)
-                        case FieldType.LONG => StructField(f.getName, LongType, nullable = true)
-                        case FieldType.FLOAT => StructField(f.getName, FloatType, nullable = true)
-                        case FieldType.DOUBLE => StructField(f.getName, DoubleType, nullable = true)
-                        case FieldType.STRING => StructField(f.getName, StringType, nullable = true)
-                        case _ => StructField(f.getName, StringType, nullable = true)
-                    }
-                })
-            )
-        }
+            val deserializer = getSerializer(
+                processorChainContext.getProperty(KafkaRecordStream.INPUT_SERIALIZER).asString,
+                processorChainContext.getProperty(KafkaRecordStream.AVRO_INPUT_SCHEMA).asString)
 
 
 
-        val sdf = new SimpleDateFormat("yyyy-MM-dd")
-        val today = sdf.format(new Date())
-        val outPath = engineContext.getProperty(HdfsBurnerEngine.OUTPUT_FOLDER_PATH).asString() + s"/day=$today"
-        val recordsDF = scala.collection.mutable.MutableList[(String, StructType, RDD[Row])]()
 
-        // group records by type
-        val recordsByType = rdd.mapPartitions(p => deserializeEvents(p, deserializer).iterator)
-            .map(record => (record.getField(FieldDictionary.RECORD_TYPE).asString(), record))
-            .groupByKey(40)
+            def convertToRow(record: Record): Row = {
+                Row.fromSeq(
+                    record.getAllFieldsSorted.toArray(Array[Field]()).map(f => {
+                        f.getType match {
+                            case FieldType.INT => f.asInteger().intValue()
+                            case FieldType.LONG => f.asLong().longValue()
+                            case FieldType.FLOAT => f.asFloat().floatValue()
+                            case FieldType.DOUBLE => f.asDouble().doubleValue()
+                            case FieldType.STRING => f.asString()
+                            case _ => f.asString()
+                        }
+                    }).toSeq
+                )
+            }
 
-        // for each disctinct record type
-        recordsByType.map(_._1)
-            .distinct(40)
-            .collect()
-            .foreach(recordType => {
-                // get all records of this type
-                val records = recordsByType.filter(_._1 == recordType).flatMap(_._2)
-                if (!records.isEmpty()) {
-                    // compute a schema from the first record
-                    val schema = convertFieldsNameToSchema(records.take(1)(0))
-
-                    // convert each Record to a Row
-                    val recordRows = records.map(r => convertToRow(r))
-                    recordsDF += ((recordType, schema, recordRows))
-                }
-
-            })
+            def convertFieldsNameToSchema(record: Record): StructType = {
+                StructType(
+                    record.getAllFieldsSorted.toArray(Array[Field]()).map(f => {
+                        f.getType match {
+                            case FieldType.INT => StructField(f.getName, IntegerType, nullable = true)
+                            case FieldType.LONG => StructField(f.getName, LongType, nullable = true)
+                            case FieldType.FLOAT => StructField(f.getName, FloatType, nullable = true)
+                            case FieldType.DOUBLE => StructField(f.getName, DoubleType, nullable = true)
+                            case FieldType.STRING => StructField(f.getName, StringType, nullable = true)
+                            case _ => StructField(f.getName, StringType, nullable = true)
+                        }
+                    })
+                )
+            }
 
 
-        recordsDF.foreach(r => {
-            sqlContext.createDataFrame(r._3, r._2)
+
+            val sdf = new SimpleDateFormat("yyyy-MM-dd")
+            val today = sdf.format(new Date())
+            val outPath = engineContext.getProperty(HdfsBurnerEngine.OUTPUT_FOLDER_PATH).asString() + s"/day=$today"
+            /*    val recordsDF = scala.collection.mutable.MutableList[(String, StructType, RDD[Row])]()
+
+         // group records by type
+              val recyType = rdd.mapPartitions(p => deserializeEvents(p, deserializer).iterator)
+                  .map(record => (record.getField(FieldDictionary.RECORD_TYPE).asString(), record))
+                  .groupByKey(40)
+
+              // for each disctinct record type
+              recordsByType.map(_._1)
+                  .distinct(40)
+                  .collect()
+                  .foreach(recordType => {
+                      // get all records of this type
+                      val records = recordsByType.filter(_._1 == recordType).flatMap(_._2)
+                      if (!records.isEmpty()) {
+                          // compute a schema from the first record
+                          val schema = convertFieldsNameToSchema(records.take(1)(0))
+
+                          // convert each Record to a Row
+                          val recordRows = records.map(r => convertToRow(r))
+                          recordsDF += ((recordType, schema, recordRows))
+                      }
+
+                  })
+
+
+              recordsDF.foreach(r => {
+                  sqlContext.createDataFrame(r._3, r._2)
+                      .write
+                //      .partitionBy(FieldDictionary.RECORD_TYPE)
+                      .mode(SaveMode.Append)
+                      // TODO choose output format
+                      .parquet(outPath)
+              })
+
+        */
+
+            val records = rdd.mapPartitions(p => deserializeEvents(p, deserializer).iterator)
+            val rows = records.map(r => convertToRow(r))
+            val schema = convertFieldsNameToSchema(records.take(1)(0))
+
+            sqlContext.createDataFrame(rows, schema)
                 .write
-          //      .partitionBy(FieldDictionary.RECORD_TYPE)
+                //      .partitionBy(FieldDictionary.RECORD_TYPE)
                 .mode(SaveMode.Append)
                 // TODO choose output format
                 .parquet(outPath)
-        })
 
 
-
-
-        // TODO save offsets ranges
+            // TODO save offsets ranges
+        }
     }
 
 }
