01793c94159d0c1b39e374d2fc3d1e8aaab92e81
Mathieu Rossignol <mathieu.rossignol@hurence.com>
2016-12-22 12:04:01 +0100

    Removing process method with one record as parameter in python basic processor

--- logisland-plugins/logisland-scripting-processors-plugin/src/main/python/processors/basic/BasicProcessor.py
+++ logisland-plugins/logisland-scripting-processors-plugin/src/main/python/processors/basic/BasicProcessor.py
@@ -27,14 +27,3 @@ class BasicProcessor(AbstractProcessor):
             copyRecord.setStringField('python_field', 'python_field_value')
             outputRecords.append(copyRecord)
         return outputRecords
-
-# same-name method with different parameters in python seems impossible or at 
-# least tricky. See http://stackoverflow.com/questions/22377338/how-to-write-same-name-methods-with-different-parameters-in-python
-# So for the moment juste implementing multi records version. Otherwise the latest
-# defined seems to be called. In clear, if you uncomment this second process method,
-# this one will be called instead of multi records version. 
-# So for the moment, only using multi records version (mono call is multi version with one item).
-# One could also decide call two distincts method processRecord and processRecords
-#
-#    def process(self, context, record):
-#        print "Inside process mono record of MyProcessor python code"
