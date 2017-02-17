af757c3fe62e1d0b60f8fd45a9f01ba32f881f8b
Mathieu Rossignol <mathieu.rossignol@hurence.com>
2017-02-17 17:54:48 +0100

    Keep first working version of injection in ES (not finished)

--- logisland-plugins/logisland-bro-plugin/src/main/java/com/hurence/logisland/processor/bro/BroProcessor.java
+++ logisland-plugins/logisland-bro-plugin/src/main/java/com/hurence/logisland/processor/bro/BroProcessor.java
@@ -15,22 +15,24 @@
  */
 package com.hurence.logisland.processor.bro;
 
-import com.google.common.collect.Lists;
 import com.hurence.logisland.annotation.documentation.CapabilityDescription;
 import com.hurence.logisland.annotation.documentation.Tags;
 import com.hurence.logisland.component.PropertyDescriptor;
 import com.hurence.logisland.processor.*;
+import com.hurence.logisland.record.FieldDictionary;
+import com.hurence.logisland.record.FieldType;
 import com.hurence.logisland.record.Record;
-import com.hurence.logisland.validator.StandardValidators;
-import com.hurence.logisland.validator.ValidationContext;
-import com.hurence.logisland.validator.ValidationResult;
+import com.hurence.logisland.util.string.JsonUtil;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import static java.util.stream.Collectors.joining;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Bro (https://www.bro.org/) processor
@@ -45,10 +47,42 @@ import java.util.List;
 public class BroProcessor extends AbstractProcessor {
 
     private static Logger logger = LoggerFactory.getLogger(BroProcessor.class);
+    
+    private Map<String, String> broFieldToLogislandField = new HashMap<String, String>();
+    
+    // Record field name for the destination ES index type
+    private static final String ES_TYPE_FIELD = "record_es_type"; 
+    
+    // JSON Field name for the bro event type
+    private static final String BRO_EVENT_TYPE = "bro_event_type";
+    
+    // Bro conn fields
+    private static final String BRO_CONN_ID_ORIG_H = "id.orig_h";
+    private static final String BRO_CONN_ID_RESP_H = "id.resp_h";
+    private static final String BRO_CONN_ID_ORIG_P = "id.orig_p";
+    private static final String BRO_CONN_ID_RESP_P = "id.resp_p";
+    
+    // Logisland conn fields
+    
+    private static final String LOGISLAND_CONN_SOURCE_IP = "source_ip";
+    private static final String LOGISLAND_CONN_DEST_IP = "dest_ip";
+    private static final String LOGISLAND_CONN_SOURCE_PORT = "source_port";
+    private static final String LOGISLAND_CONN_DEST_PORT = "dest_port";
 
     @Override
     public void init(final ProcessContext context)
     {
+        logger.debug("Initializing Bro Processor");
+        
+        // TODO add a config property to allow changing thids default bro fields mapping
+
+        // Bro conn fields mapping
+        broFieldToLogislandField.put(BRO_CONN_ID_ORIG_H, LOGISLAND_CONN_SOURCE_IP);
+        broFieldToLogislandField.put(BRO_CONN_ID_RESP_H, LOGISLAND_CONN_DEST_IP);
+        broFieldToLogislandField.put(BRO_CONN_ID_ORIG_P, LOGISLAND_CONN_SOURCE_PORT);
+        broFieldToLogislandField.put(BRO_CONN_ID_RESP_P, LOGISLAND_CONN_DEST_PORT);
+        
+        logger.debug("################################### " + broFieldToLogislandField);
     }
     
     @Override
@@ -60,20 +94,100 @@ public class BroProcessor extends AbstractProcessor {
     @Override
     public Collection<Record> process(ProcessContext context, Collection<Record> records)
     {
-        logger.debug("Processed records ");
+        logger.debug("Bro Processor records " + records);
 
+        /**
+         * Get the original Bro event as a JSON string and do some adaptation like
+         * changing some field names:
+         * - Bro field names with '.' are not acceptable for indexing into ES. Change them with something more
+         * user friendly if needed but in any case, replace the ES unwanted characters like '.'.
+         */
+        for (Record record : records)
+        {
+            String recordValue = (String)record.getField(FieldDictionary.RECORD_VALUE).getRawValue();
+            
+            // Parse as JSON object
+            Map<String, Object> jsonBroEvent = JsonUtil.convertJsonToMap(recordValue);
 
+            if (jsonBroEvent.isEmpty())
+            {
+                logger.error("Empty Bro event or error while parsing it: " + record);
+                continue;
+            }
+            
+            if (jsonBroEvent.size() != 1)
+            {
+                logger.error("Bro event should have one bro event type field: " + record);
+                continue;
+            }
+            
+            Map.Entry<String, Object> eventTypeAndValue = jsonBroEvent.entrySet().iterator().next();
+            
+            String broEventType = eventTypeAndValue.getKey();
+            Object broEventValue = eventTypeAndValue.getValue();
+            
+            Map<String, Object> finalBroEvent = null; 
+            try {
+                finalBroEvent = (Map<String, Object>)broEventValue;
+            } catch(Throwable t)
+            {
+                logger.error("Cannot understand bro event content: " + record);
+                continue;
+            }
+            
+            finalBroEvent = replaceKeys(finalBroEvent, broFieldToLogislandField);
+            
+            // Add bro event type in event payload
+            finalBroEvent.put(BRO_EVENT_TYPE, broEventType);
+            
+            String newRecordValue = JsonUtil.convertToJson(finalBroEvent);
+             
+            logger.debug("newRecordValue: " + newRecordValue);
+            record.setStringField(FieldDictionary.RECORD_VALUE, newRecordValue);
+            // Add special record field to indicate to ES processor which index type to use (index type is the bro event type)
+            record.setStringField(ES_TYPE_FIELD, broEventType);
+            
+            logger.debug("new Bro record: " + record);
+        }
 
-        Collection<Record> outputRecords = null;
-      
+        logger.debug("################## Bro Processor output records " + records);
+        return records;
+    }
+    
+    /**
+     * Given a key mapping, replaces a key with another one, in a json object
+     * @param json
+     * @param oldToNewKey
+     */
+    private static Map<String, Object> replaceKeys(Map<String, Object> json, Map<String, String> oldToNewKeys)
+    {        
+        // Now replace any forbidden character in a key name. For the moment only the '.' character is identified as
+        // a forbidden key name (for ES indexing)
+        Map<String, Object> newJson = new HashMap<String, Object>();
+        for (Map.Entry<String, Object> jsonEntry : json.entrySet())
+        {
+            String key = jsonEntry.getKey();
+            Object value = jsonEntry.getValue();
+            // Is it a key to replace ?
+            if (oldToNewKeys.containsKey(key))
+            {
+                String newKey = oldToNewKeys.get(key);
+                logger.debug("Replacing special " + key + " key with " + newKey);
+                newJson.put(newKey, value);
+            } else
+            {
+                // Not a special key to replace but we must at least remove unwanted characters
+                String newKey = key.replaceAll("\\.", "_");
+                newJson.put(newKey, value);
+            }
+        }
         
-
-        return outputRecords;
+        return newJson;
     }
     
     @Override
     public void onPropertyModified(PropertyDescriptor descriptor, String oldValue, String newValue) {
 
-        logger.info("property {} value changed from {} to {}", descriptor.getName(), oldValue, newValue);              
+        logger.debug("property {} value changed from {} to {}", descriptor.getName(), oldValue, newValue);              
     }   
 }
