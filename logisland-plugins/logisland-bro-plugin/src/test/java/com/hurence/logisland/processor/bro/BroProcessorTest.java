79e21f6183167e16fb03b9a34c38575ca74d865e
Mathieu Rossignol <mathieu.rossignol@hurence.com>
2017-02-20 17:14:47 +0100

    Added unit test for a Bro DNS event

--- logisland-plugins/logisland-bro-plugin/src/test/java/com/hurence/logisland/processor/bro/BroProcessorTest.java
+++ logisland-plugins/logisland-bro-plugin/src/test/java/com/hurence/logisland/processor/bro/BroProcessorTest.java
@@ -24,6 +24,7 @@ import com.hurence.logisland.util.runner.TestRunners;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -36,7 +37,6 @@ import org.slf4j.LoggerFactory;
 public class BroProcessorTest {
     
     private static Logger logger = LoggerFactory.getLogger(BroProcessorTest.class);
-    
     
     // Bro conn input event
     private static final String BRO_CONN_EVENT =
@@ -61,6 +61,109 @@ public class BroProcessorTest {
                 "\"tunnel_parents\": []" +
             "}" +
         "}";
+
+    // Bro dns input event
+    private static final String BRO_DNS_EVENT =
+        "{" +
+            "\"dns\": {" +
+                "\"ts\": 1487603382.840372," +
+                "\"uid\": \"Csevsb0Kzff0gvXDe\"," +
+                "\"id.orig_h\": \"172.17.0.3\"," +
+                "\"id.orig_p\": 49215," +
+                "\"id.resp_h\": \"8.8.8.8\"," +
+                "\"id.resp_p\": 53," +
+                "\"proto\": \"udp\"," +
+                "\"trans_id\": 26310," +
+                "\"query\": \"www.yahoo.fr\"," +
+                "\"rcode\": 0," +
+                "\"rcode_name\": \"NOERROR\"," +
+                "\"AA\": false," +
+                "\"TC\": false," +
+                "\"RD\": false," +
+                "\"RA\": false," +
+                "\"Z\": 0," +
+                "\"answers\": [\"rc.yahoo.com\",\"src.g03.yahoodns.net\",\"77.238.184.150\"]," +
+                "\"TTLs\": [17.0,17.0,17.0]," +
+                "\"rejected\": false" +
+            "}" +
+        "}";
+    
+    @Test
+    public void testBroDnsEvent() {
+        final TestRunner testRunner = TestRunners.newTestRunner(new BroProcessor());
+        testRunner.assertValid();
+        Record record = new StandardRecord("bro_event");
+        record.setStringField(FieldDictionary.RECORD_VALUE, BRO_DNS_EVENT);
+        testRunner.enqueue(record);
+        testRunner.clearQueues();
+        testRunner.run();
+        testRunner.assertAllInputRecordsProcessed();
+        testRunner.assertOutputRecordsCount(1);
+
+        MockRecord out = testRunner.getOutputRecords().get(0);
+        
+        out.assertFieldExists(FieldDictionary.RECORD_TYPE);
+        out.assertFieldEquals(FieldDictionary.RECORD_TYPE, "dns");
+        
+        out.assertFieldExists("ts");
+        out.assertFieldEquals("ts", (float)1487603382.840372);
+        
+        out.assertFieldExists("uid");
+        out.assertFieldEquals("uid", "Csevsb0Kzff0gvXDe");
+
+        out.assertFieldExists("id_orig_h");
+        out.assertFieldEquals("id_orig_h", "172.17.0.3");
+        
+        out.assertFieldExists("id_orig_p");
+        out.assertFieldEquals("id_orig_p", (int)49215);
+        
+        out.assertFieldExists("id_resp_h");
+        out.assertFieldEquals("id_resp_h", "8.8.8.8");
+        
+        out.assertFieldExists("id_resp_p");
+        out.assertFieldEquals("id_resp_p", (int)53);
+        
+        out.assertFieldExists("proto");
+        out.assertFieldEquals("proto", "udp");
+        
+        out.assertFieldExists("trans_id");
+        out.assertFieldEquals("trans_id", (int)26310);
+        
+        out.assertFieldExists("query");
+        out.assertFieldEquals("query", "www.yahoo.fr");
+        
+        out.assertFieldExists("rcode");
+        out.assertFieldEquals("rcode", (int)0);
+        
+        out.assertFieldExists("rcode_name");
+        out.assertFieldEquals("rcode_name", "NOERROR");
+        
+        out.assertFieldExists("AA");
+        out.assertFieldEquals("AA", false);
+        
+        out.assertFieldExists("TC");
+        out.assertFieldEquals("TC", false);
+        
+        out.assertFieldExists("RD");
+        out.assertFieldEquals("RD", false);
+        
+        out.assertFieldExists("RA");
+        out.assertFieldEquals("RA", false);
+        
+        out.assertFieldExists("Z");
+        out.assertFieldEquals("Z", (int)0);
+        
+        out.assertFieldExists("answers");
+        List<String> answers = (List<String>)out.getField("answers").getRawValue();        
+        assertEquals(Arrays.asList("rc.yahoo.com", "src.g03.yahoodns.net", "77.238.184.150"), answers);
+        
+        out.assertFieldExists("TTLs");
+        List<String> ttls = (List<String>)out.getField("TTLs").getRawValue();        
+        assertEquals(Arrays.asList(17.0 , 17.0 , 17.0), ttls);
+        
+        out.assertFieldExists("rejected");
+        out.assertFieldEquals("rejected", false);
+    }
     
     @Test
     public void testBroConnEvent() {
@@ -126,8 +229,6 @@ public class BroProcessorTest {
         
         out.assertFieldExists("tunnel_parents");
         List<String> tunnelParents = (List<String>)out.getField("tunnel_parents").getRawValue();
-        assertEquals(0, tunnelParents.size());
-        
+        assertEquals(0, tunnelParents.size());       
     }
-
 }
