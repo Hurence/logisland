b787ad602156d20f33d495989f775e5ac7495e0c
Mathieu Rossignol <mathieu.rossignol@hurence.com>
2017-02-20 16:46:15 +0100

    Unit test for basic Bro CONN event

--- logisland-plugins/logisland-bro-plugin/src/test/java/com/hurence/logisland/processor/bro/BroProcessorTest.java
+++ logisland-plugins/logisland-bro-plugin/src/test/java/com/hurence/logisland/processor/bro/BroProcessorTest.java
@@ -15,11 +15,17 @@
  */
 package com.hurence.logisland.processor.bro;
 
+import com.hurence.logisland.record.FieldDictionary;
 import com.hurence.logisland.record.Record;
 import com.hurence.logisland.record.StandardRecord;
 import com.hurence.logisland.util.runner.MockRecord;
 import com.hurence.logisland.util.runner.TestRunner;
 import com.hurence.logisland.util.runner.TestRunners;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,13 +37,37 @@ public class BroProcessorTest {
     
     private static Logger logger = LoggerFactory.getLogger(BroProcessorTest.class);
     
+    
+    // Bro conn input event
+    private static final String BRO_CONN_EVENT =
+        "{" +
+            "\"conn\": {" +
+                "\"ts\": 1487603366.277002," +
+                "\"uid\": \"Coo3g71UUMM2AyxWB\"," +
+                "\"id.orig_h\": \"172.17.0.3\"," +
+                "\"id.orig_p\": 9200," +
+                "\"id.resp_h\": \"172.17.0.2\"," +
+                "\"id.resp_p\": 42770," +
+                "\"proto\": \"tcp\"," +
+                "\"conn_state\": \"OTH\"," +
+                "\"local_orig\": true," +
+                "\"local_resp\": true," +
+                "\"missed_bytes\": 0," +
+                "\"history\": \"Cc\"," +
+                "\"orig_pkts\": 0," +
+                "\"orig_ip_bytes\": 0," +
+                "\"resp_pkts\": 0," +
+                "\"resp_ip_bytes\": 0," +
+                "\"tunnel_parents\": []" +
+            "}" +
+        "}";
+    
     @Test
-    public void testSimple() {
+    public void testBroConnEvent() {
         final TestRunner testRunner = TestRunners.newTestRunner(new BroProcessor());
-        //testRunner.setProperty(PythonProcessor.SCRIPT_CODE_PROCESS, CODE_PROCESS);
         testRunner.assertValid();
-        Record record = new StandardRecord("simple_record");
-        record.setStringField("java_field", "java_field_value");
+        Record record = new StandardRecord("bro_event");
+        record.setStringField(FieldDictionary.RECORD_VALUE, BRO_CONN_EVENT);
         testRunner.enqueue(record);
         testRunner.clearQueues();
         testRunner.run();
@@ -45,12 +75,59 @@ public class BroProcessorTest {
         testRunner.assertOutputRecordsCount(1);
 
         MockRecord out = testRunner.getOutputRecords().get(0);
-        out.assertFieldExists("java_field");
-        out.assertFieldEquals("java_field", "java_field_value");
-        // The python_field is added when processing the record from python code
-        out.assertFieldExists("python_field");
-        out.assertFieldEquals("python_field", "python_field_value");
-        out.assertRecordSizeEquals(2);
+        
+        out.assertFieldExists(FieldDictionary.RECORD_TYPE);
+        out.assertFieldEquals(FieldDictionary.RECORD_TYPE, "conn");
+        
+        out.assertFieldExists("ts");
+        out.assertFieldEquals("ts", (float)1487603366.277002);
+        
+        out.assertFieldExists("uid");
+        out.assertFieldEquals("uid", "Coo3g71UUMM2AyxWB");
+
+        out.assertFieldExists("id_orig_h");
+        out.assertFieldEquals("id_orig_h", "172.17.0.3");
+        
+        out.assertFieldExists("id_orig_p");
+        out.assertFieldEquals("id_orig_p", (int)9200);
+        
+        out.assertFieldExists("id_resp_h");
+        out.assertFieldEquals("id_resp_h", "172.17.0.2");
+        
+        out.assertFieldExists("id_resp_p");
+        out.assertFieldEquals("id_resp_p", (int)42770);
+        
+        out.assertFieldExists("proto");
+        out.assertFieldEquals("proto", "tcp");
+        
+        out.assertFieldExists("conn_state");
+        out.assertFieldEquals("conn_state", "OTH");
+        
+        out.assertFieldExists("local_orig");
+        out.assertFieldEquals("local_orig", true);
+        
+        out.assertFieldExists("local_resp");
+        out.assertFieldEquals("local_resp", true);
+        
+        out.assertFieldExists("missed_bytes");
+        out.assertFieldEquals("missed_bytes", (int)0);
+        
+        out.assertFieldExists("history");
+        out.assertFieldEquals("history", "Cc");
+        
+        out.assertFieldExists("orig_pkts");
+        out.assertFieldEquals("orig_pkts", (int)0);
+        
+        out.assertFieldExists("orig_ip_bytes");
+        out.assertFieldEquals("orig_ip_bytes", (int)0);
+
+        out.assertFieldExists("resp_ip_bytes");
+        out.assertFieldEquals("resp_ip_bytes", (int)0);
+        
+        out.assertFieldExists("tunnel_parents");
+        List<String> tunnelParents = (List<String>)out.getField("tunnel_parents").getRawValue();
+        assertEquals(0, tunnelParents.size());
+        
     }
 
 }
