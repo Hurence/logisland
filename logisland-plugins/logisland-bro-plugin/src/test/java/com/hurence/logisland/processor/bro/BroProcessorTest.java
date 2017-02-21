d21ad91ecbff57091f22796dacaef1a64615741a
Mathieu Rossignol <mathieu.rossignol@hurence.com>
2017-02-21 16:07:03 +0100

    Enriched BroProcessorTests: added Notice test and Deep JSON event test.

--- logisland-plugins/logisland-bro-plugin/src/test/java/com/hurence/logisland/processor/bro/BroProcessorTest.java
+++ logisland-plugins/logisland-bro-plugin/src/test/java/com/hurence/logisland/processor/bro/BroProcessorTest.java
@@ -26,6 +26,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -87,6 +88,146 @@ public class BroProcessorTest {
                 "\"rejected\": false" +
             "}" +
         "}";
+
+    // Bro SSH password guessing input notice
+    private static final String BRO_SSH_PASSWORD_GUESSING_NOTICE =
+        "{" +
+            "\"notice\": {" +
+                "\"ts\": 1320435875.879278," +
+                "\"note\": \"SSH::Password_Guessing\"," +
+                "\"msg\": \"172.16.238.1 appears to be guessing SSH passwords (seen in 30 connections).\"," +
+                "\"sub\": \"Sampled servers:  172.16.238.136, 172.16.238.136, 172.16.238.136, 172.16.238.136, 172.16.238.136\"," +
+                "\"src\": \"172.16.238.1\"," +
+                "\"peer_descr\": \"bro\"," +
+                "\"actions\": [\"Notice::ACTION_LOG\"]," +
+                "\"suppress_for\": 3600.0," +
+                "\"dropped\": false" +
+            "}" +
+        "}";
+    
+    // Bro fake deep input event
+    private static final String BRO_FAKE_DEEP_EVENT =
+        "{" +
+            "\"fake\": {" +
+                "\"ts\": 27.3," +
+                "\"uid\": \"anId\"," +
+                "\"level.a1\": {\"level.a2a\": \"level.a2a.value\"," +
+                "               \"level.a2b\": \"level.a2b.value\"}," +
+                "\"int\": 123," +
+                "\"level.b1\": {\"level.b2a\": {\"level.b3a\": \"level.b3a.value\"," +
+                "                               \"level.b3b\": \"level.b3b.value\"}," +
+                "               \"level.b2b\": [\"level.b2b.value1\", \"level.b2b.value2\"]}," +
+                "\"booleanT\": true," +
+                "\"booleanF\": false," +
+                "\"float\": 123.456," +
+                "\"long\": 32345678910" +
+            "}" +
+        "}";
+    
+    /**
+     * Test fields renaming if deep JSON and also some types
+     */
+    @Test
+    public void testBroFakeDeepEvent() {
+        final TestRunner testRunner = TestRunners.newTestRunner(new BroProcessor());
+        testRunner.assertValid();
+        Record record = new StandardRecord("bro_event");
+        record.setStringField(FieldDictionary.RECORD_VALUE, BRO_FAKE_DEEP_EVENT);
+        testRunner.enqueue(record);
+        testRunner.clearQueues();
+        testRunner.run();
+        testRunner.assertAllInputRecordsProcessed();
+        testRunner.assertOutputRecordsCount(1);
+
+        MockRecord out = testRunner.getOutputRecords().get(0);
+        
+        out.assertFieldExists(FieldDictionary.RECORD_TYPE);
+        out.assertFieldEquals(FieldDictionary.RECORD_TYPE, "fake");
+        
+        out.assertFieldExists("ts");
+        out.assertFieldEquals("ts", (float)27.3);
+        
+        out.assertFieldExists("uid");
+        out.assertFieldEquals("uid", "anId");
+        
+        out.assertFieldExists("level_a1");
+        Map<String, Object> level_a1 = (Map<String, Object>)out.getField("level_a1").getRawValue();
+        String level_a2aValue = (String)level_a1.get("level_a2a");
+        assertEquals("level.a2a.value", level_a2aValue);
+        String level_a2bValue = (String)level_a1.get("level_a2b");
+        assertEquals("level.a2b.value", level_a2bValue);
+        
+        out.assertFieldExists("int");
+        out.assertFieldEquals("int", (int)123);
+        
+        out.assertFieldExists("level_b1");
+        Map<String, Object> level_b1 = (Map<String, Object>)out.getField("level_b1").getRawValue();
+        Map<String, Object> level_b2aValue = (Map<String, Object>)level_b1.get("level_b2a");
+        String level_b3aValue = (String)level_b2aValue.get("level_b3a");
+        assertEquals("level.b3a.value", level_b3aValue);
+        String level_b3bValue = (String)level_b2aValue.get("level_b3b");
+        assertEquals("level.b3b.value", level_b3bValue);
+        List<String> level_b2bValue = (List<String>)level_b1.get("level_b2b");
+        assertEquals(Arrays.asList("level.b2b.value1", "level.b2b.value2"), level_b2bValue);
+        
+        out.assertFieldExists("booleanT");
+        out.assertFieldEquals("booleanT", true);
+        
+        out.assertFieldExists("booleanF");
+        out.assertFieldEquals("booleanF", false);
+        
+        out.assertFieldExists("float");
+        out.assertFieldEquals("float", (float)123.456);
+        
+        out.assertFieldExists("long");
+        out.assertFieldEquals("long", (long)32345678910L);
+    }
+    
+    @Test
+    public void testBroSshPasswordGuessingNotice() {
+        final TestRunner testRunner = TestRunners.newTestRunner(new BroProcessor());
+        testRunner.assertValid();
+        Record record = new StandardRecord("bro_event");
+        record.setStringField(FieldDictionary.RECORD_VALUE, BRO_SSH_PASSWORD_GUESSING_NOTICE);
+        testRunner.enqueue(record);
+        testRunner.clearQueues();
+        testRunner.run();
+        testRunner.assertAllInputRecordsProcessed();
+        testRunner.assertOutputRecordsCount(1);
+
+        MockRecord out = testRunner.getOutputRecords().get(0);
+        
+        out.assertFieldExists(FieldDictionary.RECORD_TYPE);
+        out.assertFieldEquals(FieldDictionary.RECORD_TYPE, "notice");
+        
+        out.assertFieldExists("ts");
+        out.assertFieldEquals("ts", (float)1320435875.879278);
+        
+        out.assertFieldExists("note");
+        out.assertFieldEquals("note", "SSH::Password_Guessing");
+        
+        out.assertFieldExists("msg");
+        out.assertFieldEquals("msg", "172.16.238.1 appears to be guessing SSH passwords (seen in 30 connections).");
+        
+        out.assertFieldExists("sub");
+        out.assertFieldEquals("sub", "Sampled servers:  172.16.238.136, 172.16.238.136, 172.16.238.136, 172.16.238.136, 172.16.238.136");
+
+        out.assertFieldExists("src");
+        out.assertFieldEquals("src", "172.16.238.1");
+        
+        out.assertFieldExists("peer_descr");
+        out.assertFieldEquals("peer_descr", "bro");
+        
+        out.assertFieldExists("actions");
+        List<String> actions = (List<String>)out.getField("actions").getRawValue();        
+        assertEquals(Arrays.asList("Notice::ACTION_LOG"), actions);
+        
+        out.assertFieldExists("suppress_for");
+        out.assertFieldEquals("suppress_for", (float)3600.0);
+        
+        out.assertFieldExists("dropped");
+        out.assertFieldEquals("dropped", false);
+    }
     
     @Test
     public void testBroDnsEvent() {
@@ -223,6 +364,9 @@ public class BroProcessorTest {
         
         out.assertFieldExists("orig_ip_bytes");
         out.assertFieldEquals("orig_ip_bytes", (int)0);
+        
+        out.assertFieldExists("resp_pkts");
+        out.assertFieldEquals("resp_pkts", (int)0);
 
         out.assertFieldExists("resp_ip_bytes");
         out.assertFieldEquals("resp_ip_bytes", (int)0);
