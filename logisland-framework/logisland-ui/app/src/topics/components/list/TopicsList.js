500f6d7941333b60fada4623174bcc3dbed8526d
denisgarcia <den.garcia@gmail.com>
2017-03-16 00:50:15 +0100

    - allow to add / remove a topic - set default value when adding a topic

--- logisland-framework/logisland-ui/app/src/topics/components/list/TopicsList.js
+++ logisland-framework/logisland-ui/app/src/topics/components/list/TopicsList.js
@@ -14,10 +14,39 @@ function TopicsListController(TopicsDataService, $log, $scope) {
 
     self.selectedTopic = null;
 
+    vm.addTopic = addTopic;
+    vm.deleteTopic = deleteTopic;
     vm.selectTopic = selectTopic;
+
+    function addTopic() {
+        self.topics[self.topics.length] = DEFAULT_TOPIC_VALUES;
+        self.selectedTopic = self.topics[self.topics.length-1];
+    }
+
+    function deleteTopic(index) {
+        self.topics.splice(index, 1);
+    }
 
     function selectTopic(topic) {
         //$log.debug("topic selected: " + JSON.stringify(topic));
         self.selectedTopic = topic;
     }
+
+    var DEFAULT_TOPIC_VALUES = { name: "topicName",
+                                 partitions: 1,
+                                 replicationFactor: 1,
+                                 documentation: "doc",
+                                 serializer: "com.hurence.logisland.serializer.KryoSerializer",
+                                 businessTimeField: "record_time",
+                                 rowkeyField: "record_id",
+                                 recordTypeField: "record_type",
+                                 keySchema: [
+                                    { name: "key1", encrypted: false, indexed: true, persistent: true, optional: true, type: "STRING" },
+                                    { name: "key2", encrypted: false, indexed: true, persistent: true, optional: true, type: "STRING" }
+                                 ],
+                                 valueSchema: [
+                                    { name: "value1", encrypted: false, indexed: true, persistent: true, optional: true, type: "STRING" },
+                                    { name: "value2", encrypted: false, indexed: true, persistent: true, optional: true, type: "STRING" }
+                                 ]
+                                 };
 }
