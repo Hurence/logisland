7afad4fc7f1b19e3fd32fe623e6e9aa1b86ca76b
denisgarcia <den.garcia@gmail.com>
2017-03-16 00:30:07 +0100

    small cleanup

--- logisland-framework/logisland-ui/app/src/jobs/components/list/JobsList.js
+++ logisland-framework/logisland-ui/app/src/jobs/components/list/JobsList.js
@@ -8,7 +8,7 @@ export default {
   }
 };
 
-function JobsListController(JobsDataService, $log, $scope, $templateCache) {
+function JobsListController(JobsDataService, $log, $scope) {
     var vm = $scope;
     var self = this;
 
