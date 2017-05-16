7adef385d51bd0d60a65fa8f51634a3623b5ced9
tkarra <tariq.karra@gmail.com>
2017-05-16 09:59:32 +0200

    Add elasticsearch 2.4.0 client service implementation

---
+++ logisland-services/logisland-elasticsearch_2_4_0-client-service/src/test/java/com/hurence/logisland/service/elasticsearchasaservice/TestProcessor.java
@@ -0,0 +1,40 @@
+
+package com.hurence.logisland.service.elasticsearchasaservice;
+
+import com.hurence.logisland.component.PropertyDescriptor;
+import com.hurence.logisland.processor.AbstractProcessor;
+import com.hurence.logisland.processor.ProcessContext;
+import com.hurence.logisland.processor.elasticsearchasaservice.ElasticsearchClientService;
+import com.hurence.logisland.record.Record;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+
+
+public class TestProcessor extends AbstractProcessor {
+
+    public static final PropertyDescriptor ELASTICSEARCH_CLIENT_SERVICE = new PropertyDescriptor.Builder()
+            .name("elasticsearch.client.service")
+            .description("The instance of the Controller Service to use for accessing Elasticsearch.")
+            .required(true)
+            .identifiesControllerService(ElasticsearchClientService.class)
+            .build();
+
+
+    @Override
+    public List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        List<PropertyDescriptor> propDescs = new ArrayList<>();
+        propDescs.add(ELASTICSEARCH_CLIENT_SERVICE);
+        return propDescs;
+    }
+
+    @Override
+    public Collection<Record> process(ProcessContext context, Collection<Record> records) {
+        return Collections.emptyList();
+    }
+
+
+}
