958827ddffa4105700f9a4db9c21ec72449cd593
Thomas Bailet <tom@zorg.local>
2017-09-01 09:34:07 +0200

    Revert "fix processo metrics computation"
    This reverts commit 1cb0b57c946506d0f90d7dc8c2353a9dd667d014.

--- logisland-engines/logisland-spark_2_1-engine/src/main/java/com/hurence/logisland/util/spark/ProcessorMetrics.java
+++ logisland-engines/logisland-spark_2_1-engine/src/main/java/com/hurence/logisland/util/spark/ProcessorMetrics.java
@@ -70,60 +70,55 @@ public class ProcessorMetrics {
             final long untilOffset,
             final long processingDurationInMillis) {
 
-        UserMetricsSystem.gauge(metricPrefix + "incoming_messages").set(untilOffset - fromOffset);
-
-        if (incomingEvents != null) {
-            UserMetricsSystem.gauge(metricPrefix + "incoming_records").set(incomingEvents.size());
-        } else {
-            UserMetricsSystem.gauge(metricPrefix + "incoming_records").set(0);
-        }
 
         if ((outgoingEvents != null) && (outgoingEvents.size() != 0)) {
+
+            UserMetricsSystem.gauge(metricPrefix + "incoming_messages").set(untilOffset - fromOffset);
+            UserMetricsSystem.gauge(metricPrefix + "incoming_records").set(incomingEvents.size());
             UserMetricsSystem.gauge(metricPrefix + "outgoing_records").set(outgoingEvents.size());
 
             long errorCount = outgoingEvents.stream().filter(r -> r.hasField(FieldDictionary.RECORD_ERRORS)).count();
             UserMetricsSystem.gauge(metricPrefix + "errors").set(errorCount);
+            if (outgoingEvents.size() != 0) {
+                final List<Integer> recordSizesInBytes = new ArrayList<>();
+                final List<Integer> recordNumberOfFields = new ArrayList<>();
 
-            final List<Integer> recordSizesInBytes = new ArrayList<>();
-            final List<Integer> recordNumberOfFields = new ArrayList<>();
+                outgoingEvents.forEach(record -> {
+                    recordSizesInBytes.add(record.sizeInBytes());
+                    recordNumberOfFields.add(record.size());
+                });
 
-            outgoingEvents.forEach(record -> {
-                recordSizesInBytes.add(record.sizeInBytes());
-                recordNumberOfFields.add(record.size());
-            });
+                final int numberOfProcessedBytes = recordSizesInBytes.stream().mapToInt(Integer::intValue).sum();
+                final int numberOfProcessedFields = recordNumberOfFields.stream().mapToInt(Integer::intValue).sum();
 
-            final int numberOfProcessedBytes = recordSizesInBytes.stream().mapToInt(Integer::intValue).sum();
-            final int numberOfProcessedFields = recordNumberOfFields.stream().mapToInt(Integer::intValue).sum();
+                if (numberOfProcessedFields != 0) {
+                    UserMetricsSystem.gauge(metricPrefix + "bytes_per_field_average").set(numberOfProcessedBytes / numberOfProcessedFields);
+                } else {
+                    UserMetricsSystem.gauge(metricPrefix + "bytes_per_field_average").set(0);
+                }
+                if (processingDurationInMillis != 0) {
+                    UserMetricsSystem.gauge(metricPrefix + "bytes_per_second_average").set(numberOfProcessedBytes * 1000 / processingDurationInMillis);
+                    UserMetricsSystem.gauge(metricPrefix + "records_per_second_average").set(outgoingEvents.size() * 1000 / processingDurationInMillis);
+                } else {
+                    UserMetricsSystem.gauge(metricPrefix + "bytes_per_second_average").set(0);
+                    UserMetricsSystem.gauge(metricPrefix + "records_per_second_average").set(0);
+                }
 
-            if (numberOfProcessedFields != 0) {
-                UserMetricsSystem.gauge(metricPrefix + "bytes_per_field_average").set(numberOfProcessedBytes / numberOfProcessedFields);
-            } else {
-                UserMetricsSystem.gauge(metricPrefix + "bytes_per_field_average").set(0);
-            }
-            if (processingDurationInMillis != 0) {
-                UserMetricsSystem.gauge(metricPrefix + "bytes_per_second_average").set(numberOfProcessedBytes * 1000 / processingDurationInMillis);
-                UserMetricsSystem.gauge(metricPrefix + "records_per_second_average").set(outgoingEvents.size() * 1000 / processingDurationInMillis);
-            } else {
-                UserMetricsSystem.gauge(metricPrefix + "bytes_per_second_average").set(0);
-                UserMetricsSystem.gauge(metricPrefix + "records_per_second_average").set(0);
-            }
 
-            UserMetricsSystem.gauge(metricPrefix + "processed_bytes").set(numberOfProcessedBytes);
-            UserMetricsSystem.gauge(metricPrefix + "processed_fields").set(numberOfProcessedFields);
-            UserMetricsSystem.gauge(metricPrefix + "error_percentage").set((long) (100.0f * errorCount / outgoingEvents.size()));
-            UserMetricsSystem.gauge(metricPrefix + "fields_per_record_average").set(numberOfProcessedFields / outgoingEvents.size());
-            UserMetricsSystem.gauge(metricPrefix + "bytes_per_record_average").set(numberOfProcessedBytes / outgoingEvents.size());
-        } else {
-            UserMetricsSystem.gauge(metricPrefix + "outgoing_records").set(0L);
-            UserMetricsSystem.gauge(metricPrefix + "errors").set(0L);
-            UserMetricsSystem.gauge(metricPrefix + "bytes_per_field_average").set(0);
-            UserMetricsSystem.gauge(metricPrefix + "bytes_per_second_average").set(0);
-            UserMetricsSystem.gauge(metricPrefix + "records_per_second_average").set(0);
-            UserMetricsSystem.gauge(metricPrefix + "processed_bytes").set(0);
-            UserMetricsSystem.gauge(metricPrefix + "processed_fields").set(0);
-            UserMetricsSystem.gauge(metricPrefix + "error_percentage").set(0);
-            UserMetricsSystem.gauge(metricPrefix + "fields_per_record_average").set(0);
-            UserMetricsSystem.gauge(metricPrefix + "bytes_per_record_average").set(0);
+                UserMetricsSystem.gauge(metricPrefix + "processed_bytes").set(numberOfProcessedBytes);
+                UserMetricsSystem.gauge(metricPrefix + "processed_fields").set(numberOfProcessedFields);
+
+                UserMetricsSystem.gauge(metricPrefix + "error_percentage").set((long) (100.0f * errorCount / outgoingEvents.size()));
+                UserMetricsSystem.gauge(metricPrefix + "fields_per_record_average").set(numberOfProcessedFields / outgoingEvents.size());
+                UserMetricsSystem.gauge(metricPrefix + "bytes_per_record_average").set(numberOfProcessedBytes / outgoingEvents.size());
+            } else if (errorCount > 0)
+                UserMetricsSystem.gauge(metricPrefix + "error_percentage").set(100L);
+            else
+                UserMetricsSystem.gauge(metricPrefix + "error_percentage").set(0L);
+
+
+            //   UserMetricsSystem.gauge(metricPrefix + "processing_time_ms").set(processingDurationInMillis);
+
         }
     }
 }
