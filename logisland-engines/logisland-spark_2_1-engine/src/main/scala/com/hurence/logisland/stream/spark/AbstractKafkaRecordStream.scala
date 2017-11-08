2ca5a5068b69acf10b4eb187e022c535a9f63ad9
Thomas Bailet <tom@zorg.espace-barral.local>
2017-11-08 16:10:53 +0100

    augment session timeout to avoid commit failed exception
    https://issues.apache.org/jira/browse/KAFKA-5167

--- logisland-engines/logisland-spark_2_1-engine/src/main/scala/com/hurence/logisland/stream/spark/AbstractKafkaRecordStream.scala
+++ logisland-engines/logisland-spark_2_1-engine/src/main/scala/com/hurence/logisland/stream/spark/AbstractKafkaRecordStream.scala
@@ -408,7 +408,7 @@ abstract class AbstractKafkaRecordStream extends AbstractRecordStream with Kafka
                 ConsumerConfig.RETRY_BACKOFF_MS_CONFIG -> "100",
                 ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> kafkaOffset,
                 ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG -> "false",
-                ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG -> s"${Integer.MAX_VALUE}"
+                ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG -> "30000"
                 /*,
                 ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG -> "5000"*/
             )
