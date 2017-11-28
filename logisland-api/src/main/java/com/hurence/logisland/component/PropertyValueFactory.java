eb8ef6369aefab881f55ae87d8acbacac175a8ad
Thomas Bailet <tom@zorg.espace-barral.local>
2017-11-28 17:20:12 +0100

    implement CSVKeyVAlue cache service

--- logisland-api/src/main/java/com/hurence/logisland/component/PropertyValueFactory.java
+++ logisland-api/src/main/java/com/hurence/logisland/component/PropertyValueFactory.java
@@ -31,7 +31,7 @@ public class PropertyValueFactory {
             final String rawValue,
             final ControllerServiceLookup controllerServiceLookup,
             final VariableRegistry variableRegistry) {
-        if (descriptor.isExpressionLanguageSupported() && InterpreterEngine.isExpressionLanguage(rawValue)) {
+        if (descriptor != null && descriptor.isExpressionLanguageSupported() && InterpreterEngine.isExpressionLanguage(rawValue)) {
             InterpreterEngine ie = InterpreterEngineFactory.get();
 
             return new InterpretedPropertyValue(descriptor, rawValue, controllerServiceLookup, variableRegistry);
