ca4b78cc82e6a1edd02dd9f57944e4f7ab53925c
arnou <jerome.arnou@gmail.com>
2018-05-30 11:01:01 +0200

    Add support for Expression language in the AddFields processor

--- logisland-plugins/logisland-common-processors-plugin/src/test/java/com/hurence/logisland/processor/AddFieldsTest.java
+++ logisland-plugins/logisland-common-processors-plugin/src/test/java/com/hurence/logisland/processor/AddFieldsTest.java
@@ -190,4 +190,119 @@ public class AddFieldsTest extends BaseSyslogTest {
         out.assertFieldEquals("string2", "value2");
     }
 
+
+    @Test
+    public void testMultipleFieldsWithExpressionLanguage() {
+
+        Record record1 = new StandardRecord();
+        record1.setField("string1", FieldType.STRING, "value1");
+        record1.setField("string2", FieldType.STRING, "value_2G");
+        record1.setField("string3", FieldType.STRING, "value3");
+
+        TestRunner testRunner = TestRunners.newTestRunner(new AddFields());
+        testRunner.setProperty(NormalizeFields.CONFLICT_RESOLUTION_POLICY, NormalizeFields.OVERWRITE_EXISTING);
+        testRunner.setProperty("stringEL1", "${'string1:'+string1+',string2:'+string2}");
+        testRunner.setProperty("stringEL2", "" +
+                "${if( string1 == 'value11' || string1 == 'value12') return 'string1'; " +
+                "else if ( string2 contains '_' ) return 'string2';}");
+        testRunner.assertValid();
+        testRunner.enqueue(record1);
+        testRunner.run();
+        testRunner.assertAllInputRecordsProcessed();
+        testRunner.assertOutputRecordsCount(1);
+
+        MockRecord out = testRunner.getOutputRecords().get(0);
+        out.assertFieldEquals("stringEL1", "string1:value1,string2:value_2G");
+        out.assertFieldEquals("stringEL2", "string2");
+        out.assertFieldTypeEquals("stringEL1", FieldType.STRING);
+    }
+
+    @Test
+    public void testMultipleFieldsWithComplexExpressionLanguageNonNullValues() {
+
+        Record record1 = new StandardRecord();
+        record1.setField("ImportanceCode", FieldType.STRING, "9003");
+        record1.setField("B2BUnit", FieldType.STRING, "12_54");
+        record1.setField("libelle_zone", FieldType.STRING, "EST");
+
+        TestRunner testRunner = TestRunners.newTestRunner(new AddFields());
+        testRunner.setProperty(NormalizeFields.CONFLICT_RESOLUTION_POLICY, NormalizeFields.OVERWRITE_EXISTING);
+        testRunner.setProperty("category", "" +
+                "${if( ImportanceCode == '9003' || ImportanceCode == '9004') return 'affiliates'; if ( B2BUnit contains '_' ) return 'marketplace'; if ( libelle_zone != 'EST' && libelle_zone != 'OUEST' && libelle_zone != 'NORD' ) return 'subsidiaries'; else return 'integrated';}");
+        testRunner.assertValid();
+        testRunner.enqueue(record1);
+        testRunner.run();
+        testRunner.assertAllInputRecordsProcessed();
+        testRunner.assertOutputRecordsCount(1);
+
+        MockRecord out = testRunner.getOutputRecords().get(0);
+        out.assertFieldEquals("category", "affiliates");
+    }
+
+    @Test
+    public void testMultipleFieldsWithComplex2ExpressionLanguageNonNullValues() {
+
+        Record record1 = new StandardRecord();
+        record1.setField("ImportanceCode", FieldType.STRING, "9008");
+        record1.setField("B2BUnit", FieldType.STRING, "12*_*54");
+        record1.setField("libelle_zone", FieldType.STRING, "EST");
+
+        TestRunner testRunner = TestRunners.newTestRunner(new AddFields());
+        testRunner.setProperty(NormalizeFields.CONFLICT_RESOLUTION_POLICY, NormalizeFields.OVERWRITE_EXISTING);
+        testRunner.setProperty("category", "" +
+                "${if( ImportanceCode == '9003' || ImportanceCode == '9004') return 'affiliates'; if ( B2BUnit contains '*_*' ) return 'marketplace'; if ( libelle_zone != 'EST' && libelle_zone != 'OUEST' && libelle_zone != 'NORD' ) return 'subsidiaries'; else return 'integrated';}");
+        testRunner.assertValid();
+        testRunner.enqueue(record1);
+        testRunner.run();
+        testRunner.assertAllInputRecordsProcessed();
+        testRunner.assertOutputRecordsCount(1);
+
+        MockRecord out = testRunner.getOutputRecords().get(0);
+        out.assertFieldEquals("category", "marketplace");
+    }
+
+    @Test
+    public void testMultipleFieldsWithComplexExpressionLanguageWithEmptyValues() {
+
+        Record record1 = new StandardRecord();
+        record1.setField("ImportanceCode", FieldType.STRING, "");
+        record1.setField("B2BUnit", FieldType.STRING, "");
+        record1.setField("libelle_zone", FieldType.STRING, "");
+
+        TestRunner testRunner = TestRunners.newTestRunner(new AddFields());
+        testRunner.setProperty(NormalizeFields.CONFLICT_RESOLUTION_POLICY, NormalizeFields.OVERWRITE_EXISTING);
+        testRunner.setProperty("category", "" +
+                "${if( ImportanceCode == '9003' || ImportanceCode == '9004') return 'affiliates'; if ( B2BUnit contains '_' ) return 'marketplace'; if ( libelle_zone != 'EST' && libelle_zone != 'OUEST' && libelle_zone != 'NORD' ) return 'subsidiaries'; else return 'integrated';}");
+        testRunner.assertValid();
+        testRunner.enqueue(record1);
+        testRunner.run();
+        testRunner.assertAllInputRecordsProcessed();
+        testRunner.assertOutputRecordsCount(1);
+
+        MockRecord out = testRunner.getOutputRecords().get(0);
+        out.assertFieldEquals("category", "subsidiaries");
+    }
+
+
+    @Test
+    public void testMultipleFieldsWithComplexExpressionLanguageWithNullValues() {
+
+        Record record1 = new StandardRecord();
+        record1.setField("ImportanceCode", FieldType.STRING, null);
+        record1.setField("B2BUnit", FieldType.STRING, null);
+        record1.setField("libelle_zone", FieldType.STRING, null);
+
+        TestRunner testRunner = TestRunners.newTestRunner(new AddFields());
+        testRunner.setProperty(NormalizeFields.CONFLICT_RESOLUTION_POLICY, NormalizeFields.OVERWRITE_EXISTING);
+        testRunner.setProperty("category", "" +
+                "${if ( ImportanceCode != null && (ImportanceCode == '9003' || ImportanceCode == '9004')) return 'affiliates'; if ( B2BUnit contains '_' ) return 'marketplace'; if ( libelle_zone != 'EST' && libelle_zone != 'OUEST' && libelle_zone != 'NORD' ) return 'subsidiaries'; else return 'integrated';}");
+        testRunner.assertValid();
+        testRunner.enqueue(record1);
+        testRunner.run();
+        testRunner.assertAllInputRecordsProcessed();
+        testRunner.assertOutputRecordsCount(1);
+
+        MockRecord out = testRunner.getOutputRecords().get(0);
+        out.assertFieldEquals("category", "subsidiaries");
+    }
 }
