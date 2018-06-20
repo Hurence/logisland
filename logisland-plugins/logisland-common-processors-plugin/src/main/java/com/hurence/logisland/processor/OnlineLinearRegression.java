d409e8e4514e90a5a03fee8ffe57b1576b1a5768
a.ait-bachir <ali.ait-bachir@coservit.fr>
2018-06-20 17:26:26 +0200

    On line linear regression with Redis

--- logisland-plugins/logisland-common-processors-plugin/src/main/java/com/hurence/logisland/processor/OnlineLinearRegression.java
+++ logisland-plugins/logisland-common-processors-plugin/src/main/java/com/hurence/logisland/processor/OnlineLinearRegression.java
@@ -10,6 +10,7 @@ import org.apache.commons.math3.stat.regression.SimpleRegression;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Time;
 import java.util.*;
 
 @Tags({"record", "linear-regression", "prediction"})
