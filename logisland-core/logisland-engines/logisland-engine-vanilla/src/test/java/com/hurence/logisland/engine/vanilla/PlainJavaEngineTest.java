94a4e8b16e9f016f28cf314809292be62220b524
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2019-04-08 17:26:07 +0200

    Use a variable in vanilla engine test. Updated pom of psark 2.3 engine.

--- logisland-core/logisland-engines/logisland-engine-vanilla/src/test/java/com/hurence/logisland/engine/vanilla/PlainJavaEngineTest.java
+++ logisland-core/logisland-engines/logisland-engine-vanilla/src/test/java/com/hurence/logisland/engine/vanilla/PlainJavaEngineTest.java
@@ -44,6 +44,10 @@ public class PlainJavaEngineTest {
     @ClassRule
     public static final SharedKafkaTestResource sharedKafkaTestResource = new SharedKafkaTestResource();
 
+    private static final String TOPIC_INPUT_1 = "topic.in";
+    private static final String TOPIC_OUTPUT_1 = "topic.out1";
+    private static final String TOPIC_OUTPUT_2 = "topic.out2";
+
 
     private EngineConfiguration engineConfiguration() {
         EngineConfiguration engineConfiguration = new EngineConfiguration();
@@ -66,8 +70,8 @@ public class PlainJavaEngineTest {
             Map<String, String> conf = new HashMap<>(props);
             conf.put("application.id", "test");
             conf.put("bootstrap.servers", sharedKafkaTestResource.getKafkaConnectString());
-            conf.put(KafkaStreamProperties.READ_TOPICS.getName(), "topic.in");
-            conf.put(KafkaStreamProperties.WRITE_TOPICS.getName(), "topic.out1,topic.out2");
+            conf.put(KafkaStreamProperties.READ_TOPICS.getName(), TOPIC_INPUT_1);
+            conf.put(KafkaStreamProperties.WRITE_TOPICS.getName(), TOPIC_OUTPUT_1 + "," + TOPIC_OUTPUT_2);
             return conf;
         };
     }
@@ -119,23 +123,23 @@ public class PlainJavaEngineTest {
         EngineConfiguration engineConfiguration = engineConfiguration();
         Map<String, String> props = new HashMap<>();
         props.put(KafkaStreamProperties.READ_TOPICS_SERIALIZER.getName(), KafkaStreamProperties.STRING_SERIALIZER.getValue());
-        props.put(KafkaStreamProperties.WRITE_TOPICS_SERIALIZER.getName(), KafkaStreamProperties.JSON_SERIALIZER.getValue());
+        props.put(KafkaStreamProperties.WRITE_TOPICS_SERIALIZER.getName(), KafkaStreamProperties.EXTENDED_JSON_SERIALIZER.getValue());
         StreamConfiguration streamConfiguration = emptyKafkaStream(defaultPropertySupplier(props));
         streamConfiguration.addProcessorConfiguration(processorConfiguration(Collections.emptyMap()));
         engineConfiguration.addPipelineConfigurations(streamConfiguration);
         EngineContext engineContext = ComponentFactory.getEngineContext(engineConfiguration).get();
         Assert.assertTrue(engineContext.isValid());
         engineContext.getEngine().start(engineContext);
-        sharedKafkaTestResource.getKafkaTestUtils().createTopic("topic.in", 1, (short) 1);
-        sharedKafkaTestResource.getKafkaTestUtils().createTopic("topic.out1", 1, (short) 1);
-        sharedKafkaTestResource.getKafkaTestUtils().createTopic("topic.out2", 1, (short) 1);
+        sharedKafkaTestResource.getKafkaTestUtils().createTopic(TOPIC_INPUT_1, 1, (short) 1);
+        sharedKafkaTestResource.getKafkaTestUtils().createTopic(TOPIC_OUTPUT_1, 1, (short) 1);
+        sharedKafkaTestResource.getKafkaTestUtils().createTopic(TOPIC_OUTPUT_2, 1, (short) 1);
 
-        List<ProducedKafkaRecord<byte[], byte[]>> inRecords = sharedKafkaTestResource.getKafkaTestUtils().produceRecords(100, "topic.in", 0);
-        List<ConsumerRecord<byte[], byte[]>> outRecords = sharedKafkaTestResource.getKafkaTestUtils().consumeAllRecordsFromTopic("topic.out1");
-        List<ConsumerRecord<byte[], byte[]>> outRecords2 = sharedKafkaTestResource.getKafkaTestUtils().consumeAllRecordsFromTopic("topic.out2");
+        List<ProducedKafkaRecord<byte[], byte[]>> inRecords = sharedKafkaTestResource.getKafkaTestUtils().produceRecords(100, TOPIC_INPUT_1, 0);
+        List<ConsumerRecord<byte[], byte[]>> outRecords = sharedKafkaTestResource.getKafkaTestUtils().consumeAllRecordsFromTopic(TOPIC_OUTPUT_1);
+        List<ConsumerRecord<byte[], byte[]>> outRecords2 = sharedKafkaTestResource.getKafkaTestUtils().consumeAllRecordsFromTopic(TOPIC_OUTPUT_2);
         Assert.assertEquals(inRecords.size(), outRecords.size());
         Assert.assertEquals(inRecords.size(), outRecords2.size());
-        RecordSerializer recordSerializer = SerializerProvider.getSerializer(KafkaStreamProperties.JSON_SERIALIZER.getValue(), null);
+        RecordSerializer recordSerializer = SerializerProvider.getSerializer(KafkaStreamProperties.EXTENDED_JSON_SERIALIZER.getValue(), null);
         for (int i = 0; i < outRecords.size(); i++) {
             ConsumerRecord<byte[], byte[]> cr = outRecords.get(i);
             ProducedKafkaRecord<byte[], byte[]> ir = inRecords.get(i);
