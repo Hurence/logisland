eda7d926d7146df5a026dde1b95d1538df30582a
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2019-06-13 16:53:02 +0200

    Fixed a dependance issue (log libraries). And updated component.sh script which was not working anymore. Updated pluginmanager to add new plugins in lib/plugins/ instead of lib/, updated code to find home as well.

--- logisland-resources/src/main/resources/bin/components.sh
+++ logisland-resources/src/main/resources/bin/components.sh
@@ -3,6 +3,9 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 LOGISLAND_HOME=$(dirname ${CURRENT_DIR})
 
+
+echo "CURRENT_DIR: ${CURRENT_DIR}"
+echo "LOGISLAND_HOME: ${LOGISLAND_HOME}"
 # Find the java binary
 if [ -n "${JAVA_HOME}" ]; then
   RUNNER="${JAVA_HOME}/bin/java"
@@ -15,13 +18,41 @@ else
   fi
 fi
 
-
-for i in $(ls "${LOGISLAND_HOME}"/lib/*.jar); do
-    CLASSPATH=$CLASSPATH:$i
-done
-
-CLASSPATH=`echo $CLASSPATH | cut -c2-`
+echo "RUNNER: ${RUNNER}"
 
 
+# update $java_cp so that it contains all logisland jars except for engines.
+# we look for jars into specified dir recursively.
+# We determine which engine jar to load latter.
+#
+# param 1 the dir where to look for jars.
+initJavaCpOptRecursively() {
+    local -r dir="${1}"
+    for entry in "$dir"/*
+    do
+      local name=$(basename -- "${entry}")
+      if [[ ! -d "$entry" ]]
+      then
+          echo "add jar ${name}"
+          if [[ -z "${java_cp}" ]]
+          then
+            java_cp="$entry"
+          else
+            java_cp="$entry:$java_cp"
+          fi
+      else
+          if [[ ! ${name} == "engines" ]]
+          then
+            initJavaCpOptRecursively "${entry}"
+          fi
+      fi
+    done
+    return 0;
+}
 
-${RUNNER} -cp ${CLASSPATH} com.hurence.logisland.plugin.PluginManager "$@"
+java_cp=""
+initJavaCpOptRecursively "${LOGISLAND_HOME}/lib"
+
+#echo "java_cp: ${java_cp}"
+
+${RUNNER} -cp ${java_cp} com.hurence.logisland.plugin.PluginManager "$@"
