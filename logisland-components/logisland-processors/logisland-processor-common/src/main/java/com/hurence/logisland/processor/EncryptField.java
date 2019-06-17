7c33f85f312f2f1d760a282b73ac55b4ed4567fb
FeizNouri <feiznouri@gmail.com>
2019-06-17 10:07:16 +0100

    added an asymmetric algo [WIP]

--- logisland-components/logisland-processors/logisland-processor-common/src/main/java/com/hurence/logisland/processor/EncryptField.java
+++ logisland-components/logisland-processors/logisland-processor-common/src/main/java/com/hurence/logisland/processor/EncryptField.java
@@ -5,22 +5,21 @@ import com.hurence.logisland.component.PropertyDescriptor;
 import com.hurence.logisland.processor.encryption.ExempleAES;
 import com.hurence.logisland.processor.encryption.ExempleAEScbc;
 import com.hurence.logisland.processor.encryption.ExempleDES;
+import com.hurence.logisland.processor.encryption.ExempleRSA;
 import com.hurence.logisland.record.Field;
 import com.hurence.logisland.record.FieldType;
 import com.hurence.logisland.record.Record;
 import com.hurence.logisland.validator.ValidationContext;
 import com.hurence.logisland.validator.ValidationResult;
+import org.apache.commons.lang3.SerializationUtils;
 import org.apache.commons.math3.exception.NullArgumentException;
 
-import javax.crypto.Cipher;
-import java.io.IOException;
-import java.security.Key;
-import java.util.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import javax.crypto.Cipher;
+import java.io.*;
+import java.security.Key;
+import java.security.PrivateKey;
+import java.util.*;
 
 
 @DynamicProperty(name = "field to encrypt",
@@ -28,6 +27,7 @@ import java.io.ObjectOutputStream;
         value = "a default value",
         description = "encrypt the field value")
 public class EncryptField extends AbstractProcessor {
+
 
     private static final long serialVersionUID = -270933070438408174L;
 
@@ -38,19 +38,20 @@ public class EncryptField extends AbstractProcessor {
     public static final String DECRYPT_MODE = "Decrypt";
     public static final String AES = "AES";
     public static final String AES_CBC_PKCS5 = "AES/CBC/PKCS5Padding";
-    /*public static final String AES_CBC_NoPAD = "AES/CBC/NoPadding";
-    public static final String AES_ECB_NoPAD = "AES/ECB/NoPadding";*/
+    public static final String AES_CBC_NoPAD = "AES/CBC/NoPadding";
+    public static final String AES_ECB_NoPAD = "AES/ECB/NoPadding";
     public static final String AES_ECB_PKCS5 = "AES/ECB/PKCS5Padding";
     public static final String DES = "DES";
-    /*public static final String DES_CBC_NoPAD = "DES/CBC/NoPadding";*/
+    public static final String DES_CBC_NoPAD = "DES/CBC/NoPadding";
     public static final String DES_CBC_PKCS5 = "DES/CBC/PKCS5Padding";
-    /*public static final String DES_ECB_NoPAD = "DES/ECB/NoPadding";*/
+    public static final String DES_ECB_NoPAD = "DES/ECB/NoPadding";
     public static final String DES_ECB_PKCS5 = "DES/ECB/PKCS5Padding";
     public static final String DESede = "DESede";
-    /*public static final String DESede_CBC_NoPAD = "DESede/CBC/NoPadding";*/
+    public static final String DESede_CBC_NoPAD = "DESede/CBC/NoPadding";
     public static final String DESede_CBC_PKCS5 = "DESede/CBC/PKCS5Padding";
-    /*public static final String DESede_ECB_NoPAD = "DESede/ECB/NoPadding";*/
+    public static final String DESede_ECB_NoPAD = "DESede/ECB/NoPadding";
     public static final String DESede_ECB_PKCS5 = "DESede/ECB/PKCS5Padding";
+    public static final String RSA_ECB_PKCS5 = "RSA/ECB/PKCS1Padding";
 
 
     public static final PropertyDescriptor MODE = new PropertyDescriptor.Builder()
@@ -65,7 +66,7 @@ public class EncryptField extends AbstractProcessor {
             .name("Algo")
             .description("Specifies the algorithm that the cipher will use")
             .required(true)
-            .allowableValues(AES, AES_CBC_PKCS5, AES_ECB_PKCS5, DES, DES_CBC_PKCS5, DES_ECB_PKCS5, DESede, DESede_CBC_PKCS5, DESede_ECB_PKCS5)
+            .allowableValues(AES, AES_CBC_PKCS5, AES_ECB_PKCS5, AES_ECB_NoPAD, AES_CBC_NoPAD, DES, DES_CBC_PKCS5, DES_ECB_PKCS5, DES_CBC_NoPAD, DES_ECB_NoPAD, DESede, DESede_CBC_PKCS5, DESede_ECB_PKCS5,DESede_CBC_NoPAD,DESede_ECB_NoPAD, RSA_ECB_PKCS5)
             .defaultValue(AES)
             .build();
 
@@ -159,7 +160,10 @@ public class EncryptField extends AbstractProcessor {
 
     // check if the algorithm chosen is AES, otherwaie it is DES or DESede
     private static boolean isAESAlgorithm(final String algorithm) {
-        return algorithm.startsWith("A");
+        return algorithm.startsWith("AES");
+    }
+    private static boolean isRSAAlgorithm(final String algorithm) {
+        return algorithm.startsWith("RSA");
     }
 
 
@@ -182,6 +186,21 @@ public class EncryptField extends AbstractProcessor {
                         if (!record.hasField(fieldName)) continue;
                         Field field = record.getField(fieldName);
                         try {
+                            if (isRSAAlgorithm(context.getProperty(ALGO))) {
+                                if (context.getProperty(ALGO).contains("CBC")) {
+                                    ExempleRSA encryptRSA = new ExempleRSA(context.getProperty(ALGO));
+                                    record.setField(fieldName, FieldType.BYTES, encryptRSA.encrypt(field.getRawValue(), context.getProperty(KEY))); // is field an Object ??!!
+                                    String IvName = "IV" + fieldName;
+                                    String fieldKeyName = "Key" + fieldName;
+                                    record.setField(IvName, FieldType.BYTES, encryptRSA.getiv());
+                                    record.setField(fieldKeyName, FieldType.BYTES, encryptRSA.wrapedkey());
+                                } else {
+                                    ExempleRSA encryptRSA = new ExempleRSA(context.getProperty(ALGO));
+                                    record.setField(fieldName, FieldType.BYTES, encryptRSA.encrypt(field.getRawValue(), context.getProperty(KEY))); // is field an Object ??!!
+                                    String fieldKeyName = "Key" + fieldName;
+                                    record.setField(fieldKeyName, FieldType.BYTES, encryptRSA.wrapedkey());
+                                }
+                            }
                             if (isAESAlgorithm(context.getProperty(ALGO))) {
                                 if (context.getProperty(ALGO).contains("CBC")) {
                                     ExempleAES encryptAES = new ExempleAES(context.getProperty(ALGO), context.getProperty(KEY));
@@ -203,6 +222,7 @@ public class EncryptField extends AbstractProcessor {
                                     record.setField(fieldName, FieldType.BYTES, encryptDES.encrypt(field.getRawValue())); // is field an Object ??!!
                                 }
                             }
+
                         } catch (Exception ex) {
                             getLogger().error("error while processing record field" + fieldName, ex);
                         }
@@ -216,6 +236,44 @@ public class EncryptField extends AbstractProcessor {
                         Field field = record.getField(fieldName);
                         if (!record.hasField(fieldName)) continue;
                         try {
+                            if (isRSAAlgorithm(context.getProperty(ALGO))) {
+                                /*if (context.getProperty(ALGO).contains("AES/CBC")) {
+                                    ///
+                                } else {
+                                    ExempleAES encryptAES = new ExempleAES(context.getProperty(ALGO), context.getProperty(KEY));
+                                }*/
+                                if (!field.getType().equals(FieldType.BYTES)) {
+                                    record.addError("Wrong input", getLogger(), "type was instead of");
+                                    continue;
+                                }
+                                FieldType type;
+                                try{
+                                    type = getFieldType(fieldType);
+                                } catch (IllegalArgumentException | NullPointerException ex) {
+                                    getLogger().error("error while processing record field" + fieldName +" ; ", ex);
+                                    continue;
+                                }
+                                if (context.getProperty(ALGO).contains("CBC")) {
+                                    String IvName = "IV" + fieldName;
+                                    String fieldKeyName = "Key" + fieldName;
+                                    ExempleRSA encryptRSA = new ExempleRSA(context.getProperty(ALGO),(byte[]) record.getField(IvName).getRawValue());
+                                    try {
+                                        Key key = encryptRSA.getPriKey((byte[])record.getField(fieldKeyName).getRawValue(), context.getProperty(KEY));
+                                        record.setField(fieldName, type, encryptRSA.decrypt((byte[]) field.getRawValue(), key)); // !!!!!!!!!!! how to know the original type of the field before encrypting
+                                    } catch (Exception ex) {
+                                        getLogger().error("error while setting casting value to byte array", ex);
+                                    }
+                                } else {
+                                    ExempleRSA encryptRSA = new ExempleRSA(context.getProperty(ALGO));
+                                    String fieldKeyName = "Key" + fieldName;
+                                    try {
+                                        Key key = encryptRSA.getPriKey((byte[])record.getField(fieldKeyName).getRawValue(),context.getProperty(KEY));
+                                        record.setField(fieldName, type, encryptRSA.decrypt((byte[]) field.getRawValue(), key));
+                                    } catch (Exception ex) {
+                                        getLogger().error("error while setting casting value to byte array", ex);
+                                    }
+                                }
+                            }
                             if (isAESAlgorithm(context.getProperty(ALGO))) {
                                 /*if (context.getProperty(ALGO).contains("AES/CBC")) {
                                     ///
@@ -282,6 +340,7 @@ public class EncryptField extends AbstractProcessor {
                                     }
                                 }
                             }
+
                         } catch (Exception ex) {
                             getLogger().error("error while processing record field" + fieldName, ex);
                         }
@@ -315,7 +374,6 @@ public class EncryptField extends AbstractProcessor {
         }
         return bytes;
     }
-    // convert byte array to object
     public static Object toObject(byte[] bytes) throws IOException, ClassNotFoundException {
         Object obj = null;
         ByteArrayInputStream bis = null;
@@ -334,6 +392,15 @@ public class EncryptField extends AbstractProcessor {
         }
         return obj;
     }
+    /*public static byte[] toByteArray(Object obj) throws IOException {
+        byte[] data = SerializationUtils.serialize( (Serializable) obj);
+        return data;
+    }
+    // convert byte array to object
+    public static Object toObject(byte[] bytes) throws IOException, ClassNotFoundException {
+        Object obj = SerializationUtils.deserialize(bytes);
+        return obj;
+    }*/
     // get the specific field or fields to encrypt form the dynamic property
     private Collection<String> getFieldsNameMapping(ProcessContext context) {
         Collection<String> fieldsNameMappings = new ArrayList<>();
