6264aec60a7dd4f8e5979d486193d307d3c1f214
oalam <bailet.thomas@gmail.com>
2019-06-20 09:24:32 +0200

    doc : fix batchduration useless param

--- logisland-core/logisland-engines/logisland-engine-spark_2_X/logisland-engine-spark_2_common/src/main/scala/com/hurence/logisland/engine/spark/KafkaStreamProcessingEngine.scala
+++ logisland-core/logisland-engines/logisland-engine-spark_2_X/logisland-engine-spark_2_common/src/main/scala/com/hurence/logisland/engine/spark/KafkaStreamProcessingEngine.scala
@@ -359,6 +359,7 @@ class KafkaStreamProcessingEngine extends AbstractProcessingEngine {
     private val logger = LoggerFactory.getLogger(classOf[KafkaStreamProcessingEngine])
     private val conf = new SparkConf()
     private var running = false
+    protected var batchDurationMs: Int = 1000
 
 
     /**
@@ -369,7 +370,7 @@ class KafkaStreamProcessingEngine extends AbstractProcessingEngine {
         val engineContext = context.asInstanceOf[EngineContext]
         val sparkMaster = engineContext.getPropertyValue(KafkaStreamProcessingEngine.SPARK_MASTER).asString
         val appName = engineContext.getPropertyValue(KafkaStreamProcessingEngine.SPARK_APP_NAME).asString
-        val batchDuration = engineContext.getPropertyValue(KafkaStreamProcessingEngine.SPARK_STREAMING_BATCH_DURATION).asInteger().intValue()
+        batchDurationMs = engineContext.getPropertyValue(KafkaStreamProcessingEngine.SPARK_STREAMING_BATCH_DURATION).asInteger().intValue()
 
         /**
           * job configuration
@@ -476,7 +477,7 @@ class KafkaStreamProcessingEngine extends AbstractProcessingEngine {
 
         logger.info(s"spark context initialized with master:$sparkMaster, " +
             s"appName:$appName, " +
-            s"batchDuration:$batchDuration ")
+            s"batchDuration:$batchDurationMs ")
         logger.info(s"conf : ${conf.toDebugString}")
     }
 
@@ -535,9 +536,7 @@ class KafkaStreamProcessingEngine extends AbstractProcessingEngine {
 
     protected def getCurrentSparkStreamingContext(sparkContext: SparkContext): StreamingContext = {
         return StreamingContext.getActiveOrCreate(() =>
-            return new StreamingContext(sparkContext,
-                Milliseconds(sparkContext.getConf.get(KafkaStreamProcessingEngine.SPARK_STREAMING_BATCH_DURATION.getName,
-                    KafkaStreamProcessingEngine.SPARK_STREAMING_BATCH_DURATION.getDefaultValue).toInt))
+            return new StreamingContext(sparkContext, Milliseconds(batchDurationMs))
         )
     }
 
