39605e10a11c05a7efadd9e470b97da0dcf6433a
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2019-09-27 12:09:11 +0200

    Added tests and found a missing dependency (mvel)

--- logisland-components/logisland-processors/logisland-processor-rest/src/test/java/com/hurence/logisland/rest/processor/lookup/CallRequestTest.java
+++ logisland-components/logisland-processors/logisland-processor-rest/src/test/java/com/hurence/logisland/rest/processor/lookup/CallRequestTest.java
@@ -17,11 +17,10 @@ package com.hurence.logisland.rest.processor.lookup;
 
 import com.hurence.logisland.component.InitializationException;
 import com.hurence.logisland.record.FieldType;
-import com.hurence.logisland.record.Record;
-import com.hurence.logisland.record.RecordUtils;
 import com.hurence.logisland.record.StandardRecord;
 import com.hurence.logisland.service.lookup.LookupFailureException;
 import com.hurence.logisland.service.rest.MockRestClientService;
+import com.hurence.logisland.service.rest.RestClientService;
 import com.hurence.logisland.util.runner.MockRecord;
 import com.hurence.logisland.util.runner.TestRunner;
 import com.hurence.logisland.util.runner.TestRunners;
@@ -33,6 +32,7 @@ import static com.hurence.logisland.rest.processor.lookup.CallRequest.*;
 
 public class CallRequestTest {
 
+    private static String SERVICE_ID = "restLookupService";
     @Test
     public void testCustomValidate() {
         final TestRunner runner = TestRunners.newTestRunner(new CallRequest());
@@ -63,18 +63,7 @@ public class CallRequestTest {
 
     @Test
     public void basic_test() throws InitializationException {
-        final TestRunner runner = TestRunners.newTestRunner(new CallRequest());
-        MockRestClientService service = new MockRestClientService();
-        //enable service
-        runner.addControllerService("restLookupService", service);
-        runner.enableControllerService(service);
-        runner.assertValid(service);
-
-        //config proc
-        runner.setProperty(HTTP_CLIENT_SERVICE, "restLookupService");
-        runner.setProperty(FIELD_HTTP_RESPONSE, "response");
-        runner.setProperty(CONFLICT_RESOLUTION_POLICY, KEEP_OLD_FIELD.getValue());
-        runner.assertValid();
+        final TestRunner runner = getRunnerInitialized();
 
         //test queries
         StandardRecord record1 = new StandardRecord();
@@ -107,18 +96,7 @@ public class CallRequestTest {
 
     @Test
     public void basic_test_2() throws InitializationException, IOException, LookupFailureException {
-        final TestRunner runner = TestRunners.newTestRunner(new CallRequest());
-        MockRestClientService service = new MockRestClientService();
-        //enable service
-        runner.addControllerService("restLookupService", service);
-        runner.enableControllerService(service);
-        runner.assertValid(service);
-
-        //config proc
-        runner.setProperty(HTTP_CLIENT_SERVICE, "restLookupService");
-        runner.setProperty(FIELD_HTTP_RESPONSE, "response");
-        runner.setProperty(CONFLICT_RESOLUTION_POLICY, KEEP_OLD_FIELD.getValue());
-        runner.assertValid();
+        final TestRunner runner = getRunnerInitialized();
 
         //test queries
         StandardRecord record = new StandardRecord();
@@ -139,5 +117,180 @@ public class CallRequestTest {
         coordinnates.assertFieldTypeEquals("employeeId", FieldType.STRING);
     }
 
+    @Test
+    public void adding_verb_to_coordinates() throws InitializationException, IOException, LookupFailureException {
+        final TestRunner runner = getRunnerInitialized();
+        final RestClientService service = (RestClientService) runner.getControllerService(SERVICE_ID);
+        runner.setProperty(REQUEST_METHOD, "delete");
+        runner.assertValid();
 
+        //test queries
+        StandardRecord record = new StandardRecord();
+        record.setField("employeeId", FieldType.STRING, "hello");
+        runner.enqueue(new StandardRecord(record));
+        runner.run();
+        runner.assertAllInputRecordsProcessed();
+        runner.assertOutputRecordsCount(1);
+
+        MockRecord out = runner.getOutputRecords().get(0);
+        out.assertRecordSizeEquals(2);
+        out.assertFieldEquals("employeeId", "hello");
+        out.assertFieldTypeEquals("employeeId", FieldType.STRING);
+        out.assertFieldTypeEquals("response", FieldType.RECORD);
+        MockRecord coordinates = new MockRecord(out.getField("response").asRecord());
+        coordinates.assertRecordSizeEquals(2);
+        coordinates.assertFieldEquals("employeeId", "hello");
+        coordinates.assertFieldEquals(service.getMethodKey(), "delete");
+        coordinates.assertFieldTypeEquals("employeeId", FieldType.STRING);
+    }
+
+    @Test
+    public void adding_verb_to_coordinates_expression_language() throws InitializationException, IOException, LookupFailureException {
+        final TestRunner runner = getRunnerInitialized();
+        final RestClientService service = (RestClientService) runner.getControllerService(SERVICE_ID);
+        runner.setProperty(REQUEST_METHOD, "${'delete_' + employeeId}");
+        runner.assertValid();
+
+        //test queries
+        StandardRecord record = new StandardRecord();
+        record.setField("employeeId", FieldType.STRING, "hello");
+        runner.enqueue(new StandardRecord(record));
+        runner.run();
+        runner.assertAllInputRecordsProcessed();
+        runner.assertOutputRecordsCount(1);
+
+        MockRecord out = runner.getOutputRecords().get(0);
+        out.assertRecordSizeEquals(2);
+        out.assertFieldEquals("employeeId", "hello");
+        out.assertFieldTypeEquals("employeeId", FieldType.STRING);
+        out.assertFieldTypeEquals("response", FieldType.RECORD);
+        MockRecord coordinates = new MockRecord(out.getField("response").asRecord());
+        coordinates.assertRecordSizeEquals(2);
+        coordinates.assertFieldEquals("employeeId", "hello");
+        coordinates.assertFieldEquals(service.getMethodKey(), "delete_hello");
+        coordinates.assertFieldTypeEquals("employeeId", FieldType.STRING);
+    }
+
+    @Test
+    public void adding_mime_type_coordinates() throws InitializationException, IOException, LookupFailureException {
+        final TestRunner runner = getRunnerInitialized();
+        final RestClientService service = (RestClientService) runner.getControllerService(SERVICE_ID);
+        runner.setProperty(REQUEST_MIME_TYPE, "mimtype");
+        runner.assertValid();
+
+        //test queries
+        StandardRecord record = new StandardRecord();
+        record.setField("employeeId", FieldType.STRING, "hello");
+        runner.enqueue(new StandardRecord(record));
+        runner.run();
+        runner.assertAllInputRecordsProcessed();
+        runner.assertOutputRecordsCount(1);
+
+        MockRecord out = runner.getOutputRecords().get(0);
+        out.assertRecordSizeEquals(2);
+        out.assertFieldEquals("employeeId", "hello");
+        out.assertFieldTypeEquals("employeeId", FieldType.STRING);
+        out.assertFieldTypeEquals("response", FieldType.RECORD);
+        MockRecord coordinates = new MockRecord(out.getField("response").asRecord());
+        coordinates.assertRecordSizeEquals(2);
+        coordinates.assertFieldEquals("employeeId", "hello");
+        coordinates.assertFieldEquals(service.getMimeTypeKey(), "mimtype");
+        coordinates.assertFieldTypeEquals("employeeId", FieldType.STRING);
+    }
+
+    @Test
+    public void adding_mime_type_coordinates_expression_language() throws InitializationException, IOException, LookupFailureException {
+        final TestRunner runner = getRunnerInitialized();
+        final RestClientService service = (RestClientService) runner.getControllerService(SERVICE_ID);
+        runner.setProperty(REQUEST_MIME_TYPE, "${'mimtype_' + employeeId}");
+        runner.assertValid();
+
+        //test queries
+        StandardRecord record = new StandardRecord();
+        record.setField("employeeId", FieldType.STRING, "hello");
+        runner.enqueue(new StandardRecord(record));
+        runner.run();
+        runner.assertAllInputRecordsProcessed();
+        runner.assertOutputRecordsCount(1);
+
+        MockRecord out = runner.getOutputRecords().get(0);
+        out.assertRecordSizeEquals(2);
+        out.assertFieldEquals("employeeId", "hello");
+        out.assertFieldTypeEquals("employeeId", FieldType.STRING);
+        out.assertFieldTypeEquals("response", FieldType.RECORD);
+        MockRecord coordinates = new MockRecord(out.getField("response").asRecord());
+        coordinates.assertRecordSizeEquals(2);
+        coordinates.assertFieldEquals("employeeId", "hello");
+        coordinates.assertFieldEquals(service.getMimeTypeKey(), "mimtype_hello");
+        coordinates.assertFieldTypeEquals("employeeId", FieldType.STRING);
+    }
+
+    @Test
+    public void adding_body_coordinates() throws InitializationException, IOException, LookupFailureException {
+        final TestRunner runner = getRunnerInitialized();
+        final RestClientService service = (RestClientService) runner.getControllerService(SERVICE_ID);
+        runner.setProperty(REQUEST_BODY, "body");
+        runner.assertValid();
+
+        //test queries
+        StandardRecord record = new StandardRecord();
+        record.setField("employeeId", FieldType.STRING, "hello");
+        runner.enqueue(new StandardRecord(record));
+        runner.run();
+        runner.assertAllInputRecordsProcessed();
+        runner.assertOutputRecordsCount(1);
+
+        MockRecord out = runner.getOutputRecords().get(0);
+        out.assertRecordSizeEquals(2);
+        out.assertFieldEquals("employeeId", "hello");
+        out.assertFieldTypeEquals("employeeId", FieldType.STRING);
+        out.assertFieldTypeEquals("response", FieldType.RECORD);
+        MockRecord coordinates = new MockRecord(out.getField("response").asRecord());
+        coordinates.assertRecordSizeEquals(2);
+        coordinates.assertFieldEquals("employeeId", "hello");
+        coordinates.assertFieldEquals(service.getbodyKey(), "body");
+        coordinates.assertFieldTypeEquals("employeeId", FieldType.STRING);
+    }
+
+    @Test
+    public void adding_body_coordinates_expression_language() throws InitializationException, IOException, LookupFailureException {
+        final TestRunner runner = getRunnerInitialized();
+        final RestClientService service = (RestClientService) runner.getControllerService(SERVICE_ID);
+        runner.setProperty(REQUEST_BODY, "${'body_' + employeeId}");
+        runner.assertValid();
+
+        //test queries
+        StandardRecord record = new StandardRecord();
+        record.setField("employeeId", FieldType.STRING, "hello");
+        runner.enqueue(new StandardRecord(record));
+        runner.run();
+        runner.assertAllInputRecordsProcessed();
+        runner.assertOutputRecordsCount(1);
+
+        MockRecord out = runner.getOutputRecords().get(0);
+        out.assertRecordSizeEquals(2);
+        out.assertFieldEquals("employeeId", "hello");
+        out.assertFieldTypeEquals("employeeId", FieldType.STRING);
+        out.assertFieldTypeEquals("response", FieldType.RECORD);
+        MockRecord coordinates = new MockRecord(out.getField("response").asRecord());
+        coordinates.assertRecordSizeEquals(2);
+        coordinates.assertFieldEquals("employeeId", "hello");
+        coordinates.assertFieldEquals(service.getbodyKey(), "body_hello");
+        coordinates.assertFieldTypeEquals("employeeId", FieldType.STRING);
+    }
+
+    private TestRunner getRunnerInitialized() throws InitializationException {
+        final TestRunner runner = TestRunners.newTestRunner(new CallRequest());
+        MockRestClientService service = new MockRestClientService();
+        //enable service
+        runner.addControllerService("restLookupService", service);
+        runner.enableControllerService(service);
+        runner.assertValid(service);
+        //config proc
+        runner.setProperty(HTTP_CLIENT_SERVICE, "restLookupService");
+        runner.setProperty(FIELD_HTTP_RESPONSE, "response");
+        runner.setProperty(CONFLICT_RESOLUTION_POLICY, KEEP_OLD_FIELD.getValue());
+        runner.assertValid();
+        return runner;
+    }
 }
