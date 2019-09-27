741c86b8a88cf34663576f5655fc7eb0cbaffb58
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2019-09-27 15:40:15 +0200

    Adding forgotten file...

---
+++ logisland-core/logisland-framework/logisland-utils/src/main/java/com/hurence/logisland/error/ErrorUtils.java
@@ -0,0 +1,15 @@
+package com.hurence.logisland.error;
+
+import com.hurence.logisland.logging.ComponentLog;
+import com.hurence.logisland.record.Record;
+
+public class ErrorUtils {
+    static public void handleError(final ComponentLog logger, final Exception ex, final Record record, String errorType) {
+        StringBuilder stb = new StringBuilder(ex.toString());
+        for (Throwable exception = ex.getCause(); exception != null; exception = exception.getCause()) {
+            stb.append(System.getProperty("line.separator"));
+            stb.append(exception.toString());
+        }
+        record.addError(errorType, logger, stb.toString());
+    }
+}
