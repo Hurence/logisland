2a719c5d6c8edc7a1a7a10f56b9c3b27c8098a8d
Mathieu Rossignol <mathieu.rossignol@hurence.com>
2020-02-21 15:42:21 +0100

    Introudcing support of spark standalone cluster in logisland.sh

--- logisland-resources/src/main/resources/bin/logisland.sh
+++ logisland-resources/src/main/resources/bin/logisland.sh
@@ -279,11 +279,13 @@ main() {
                    ;;
             esac
 
-
-
         export SPARK_PRINT_LAUNCH_COMMAND=1
         echo "Detected spark version ${SPARK_VERSION}. We'll automatically plug in engine jar ${engine_jar}"
         APP_NAME=`awk '{ if( $1 == "spark.app.name:" ){ print $2 } }' ${CONF_FILE}`
+
+        #
+        # YARN mode?
+        #
         MODE=`awk '{ if( $1 == "spark.master:" ){ print $2 } }' ${CONF_FILE}`
         case ${MODE} in
           "yarn")
@@ -303,31 +305,54 @@ main() {
               MODE=${MODE}-${EXTRA_MODE}
             fi
             ;;
+
         esac
 
-
+        #
+        # MESOS mode?
+        #
         if [[ "${MODE}" =~ "mesos" ]]
         then
             SPARK_MASTER=${MODE}
             MODE="mesos"
         fi
 
+        #
+        # Spark standalone mode?
+        #
+        if [[ "${MODE}" =~ ^spark://.* ]] # Starts with spark:// (spark standalone url)
+        then
+            SPARK_MASTER=${MODE}
+            EXTRA_MODE=`awk '{ if( $1 == "spark.deploy-mode:" ){ print $2 } }' ${CONF_FILE}`
+            if [[ -z "${EXTRA_MODE}" ]]
+            then
+             echo "The property \"spark.deploy-mode\" is missing in config file \"${CONF_FILE}\""
+             exit 1
+            fi
 
+            if [[ ! ${EXTRA_MODE} = "cluster" && ! ${EXTRA_MODE} = "client" ]]
+            then
+              echo "The property \"spark.deploy-mode\" value \"${EXTRA_MODE}\" is not supported"
+              exit 1
+            else
+              MODE=spark-${EXTRA_MODE}
+            fi
+        fi
 
         if [[ ! -z "${VERBOSE_OPTIONS}" ]]
         then
-          echo "Starting with mode \"${MODE}\" on master \"${SPARK_MASTER}\""
+          echo "Starting with run mode \"${MODE}\" on master \"${SPARK_MASTER}\""
         fi
 
         case ${MODE} in
           local*)
 
             ${SPARK_HOME}/bin/spark-submit ${VERBOSE_OPTIONS} ${YARN_CLUSTER_OPTIONS} \
-             --driver-library-path ${OPENCV_NATIVE_LIB_PATH} \
-             --conf spark.driver.extraJavaOptions="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Dlog4j.configuration=\"file:${lib_dir}/../conf/log4j.properties\"" \
-             --conf spark.executor.extraJavaOptions="-Dlog4j.configuration=\"file:${lib_dir}/../conf/log4j.properties\"" \
-             --conf spark.metrics.namespace="${APP_NAME}"  \
-             --conf spark.metrics.conf="${lib_dir}/../monitoring/metrics.properties"  \
+            --driver-library-path ${OPENCV_NATIVE_LIB_PATH} \
+            --conf spark.driver.extraJavaOptions="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Dlog4j.configuration=\"file:${lib_dir}/../conf/log4j.properties\"" \
+            --conf spark.executor.extraJavaOptions="-Dlog4j.configuration=\"file:${lib_dir}/../conf/log4j.properties\"" \
+            --conf spark.metrics.namespace="${APP_NAME}"  \
+            --conf spark.metrics.conf="${lib_dir}/../monitoring/metrics.properties"  \
             --class ${app_mainclass} \
             --jars ${app_classpath} ${engine_jar} \
             -conf ${CONF_FILE}
@@ -335,8 +360,7 @@ main() {
             ;;
           yarn-cluster)
 
-            YARN_CLUSTER_OPTIONS="--master yarn --deploy-mode cluster --files ${CONF_FILE}#logisland-configuration.yml,${CONF_DIR}/../monitoring/jmx_prometheus_javaagent-0.10.jar#jmx_prometheus_javaagent-0.10.jar,${CONF_DIR}/../monitoring/spark-prometheus.yml#spark-prometheus.yml,${CONF_DIR}/../monitoring/metrics.properties#metrics.properties,${CONF_DIR}/log4j.properties#log4j.properties --conf spark.metrics.namespace=\"${APP_NAME}\" --conf \"spark.executor.extraJavaOptions=-Dlog4j.configuration=log4j.properties\" --conf spark.ui.showConsoleProgress=false"
-
+            YARN_CLUSTER_OPTIONS="--master yarn --deploy-mode cluster --files ${CONF_FILE}#logisland-configuration.yml,${CONF_DIR}/../monitoring/jmx_prometheus_javaagent-0.10.jar#jmx_prometheus_javaagent-0.10.jar,${CONF_DIR}/../monitoring/spark-prometheus.yml#spark-prometheus.yml,${CONF_DIR}/../monitoring/metrics.properties#metrics.properties,${CONF_DIR}/log4j.properties#log4j.properties --conf spark.metrics.namespace=\"${APP_NAME}\" --conf spark.ui.showConsoleProgress=false"
 
             if [[ ! -z "${YARN_APP_NAME}" ]]
             then
@@ -415,7 +439,6 @@ main() {
                  YARN_CLUSTER_OPTIONS="${YARN_CLUSTER_OPTIONS} --conf spark.task.maxFailures=${SPARK_TASK_MAX_FAILURES}"
             fi
 
-
             PROPERTIES_FILE_PATH=`awk '{ if( $1 == "spark.properties.file.path:" ){ print $2 } }' ${CONF_FILE}`
             if [[ ! -z "${PROPERTIES_FILE_PATH}" ]]
             then
@@ -440,7 +463,7 @@ main() {
             --conf "${EXTRA_PROCESSOR_JAVA_OPTIONS}" \
             --class ${app_mainclass} \
             --jars ${app_classpath} ${engine_jar} \
-             -conf ${CONF_FILE}
+            -conf ${CONF_FILE}
 
             ;;
           yarn-client)
@@ -470,13 +493,12 @@ main() {
             --conf spark.metrics.conf=./metrics.properties \
             --class ${app_mainclass} \
             --jars ${app_classpath} ${engine_jar} \
-             -conf ${CONF_FILE}
+            -conf ${CONF_FILE}
             ;;
 
           mesos)
 
             MESOS_OPTIONS="--master ${SPARK_MASTER} --conf spark.metrics.namespace=\"${APP_NAME}\""
-
 
             DRIVER_CORES=`awk '{ if( $1 == "spark.driver.cores:" ){ print $2 } }' ${CONF_FILE}`
             if [[ ! -z "${DRIVER_CORES}" ]]
@@ -517,12 +539,10 @@ main() {
 
             MESOS_NATIVE_JAVA_LIBRARY=`awk '{ if( $1 == "java.library.path:" ){ print $2 } }' ${CONF_FILE}`
 
-
-
             #--deploy-mode cluster \
             #--supervise \
             #--executor-memory 20G \
-           # --total-executor-cores 100 \
+            # --total-executor-cores 100 \
 
             export MESOS_NATIVE_JAVA_LIBRARY="${MESOS_NATIVE_JAVA_LIBRARY}"
             ${SPARK_HOME}/bin/spark-submit ${VERBOSE_OPTIONS} ${MESOS_OPTIONS} \
@@ -530,6 +550,137 @@ main() {
             --class ${app_mainclass} \
             --jars ${app_classpath} ${engine_jar} \
             -conf ${CONF_FILE}
+            ;;
+
+          spark-cluster)
+            SPARK_CLUSTER_OPTIONS="--master ${SPARK_MASTER} --deploy-mode cluster --files ${CONF_FILE}#logisland-configuration.yml,${CONF_DIR}/../monitoring/jmx_prometheus_javaagent-0.10.jar#jmx_prometheus_javaagent-0.10.jar,${CONF_DIR}/../monitoring/spark-prometheus.yml#spark-prometheus.yml,${CONF_DIR}/../monitoring/metrics.properties#metrics.properties,${CONF_DIR}/log4j.properties#log4j.properties --conf spark.metrics.namespace=\"${APP_NAME}\" --conf spark.ui.showConsoleProgress=false"
+
+            if [[ ! -z "${SPARK_APP_NAME}" ]]
+            then
+              SPARK_CLUSTER_OPTIONS="${SPARK_CLUSTER_OPTIONS} --name ${SPARK_APP_NAME}"
+            else
+              SPARK_APP_NAME=`awk '{ if( $1 == "spark.app.name:" ){ print $2 } }' ${CONF_FILE}`
+              if [[ ! -z "${SPARK_APP_NAME}" ]]
+              then
+                SPARK_CLUSTER_OPTIONS="${SPARK_CLUSTER_OPTIONS} --name ${SPARK_APP_NAME}"
+              fi
+            fi
+
+            DRIVER_CORES=`awk '{ if( $1 == "spark.driver.cores:" ){ print $2 } }' ${CONF_FILE}`
+            if [[ ! -z "${DRIVER_CORES}" ]]
+            then
+             SPARK_CLUSTER_OPTIONS="${SPARK_CLUSTER_OPTIONS} --driver-cores ${DRIVER_CORES}"
+            fi
+
+            DRIVER_MEMORY=`awk '{ if( $1 == "spark.driver.memory:" ){ print $2 } }' ${CONF_FILE}`
+            if [[ ! -z "${DRIVER_MEMORY}" ]]
+            then
+             SPARK_CLUSTER_OPTIONS="${SPARK_CLUSTER_OPTIONS} --driver-memory ${DRIVER_MEMORY}"
+            fi
+
+            EXECUTORS_CORES=`awk '{ if( $1 == "spark.executor.cores:" ){ print $2 } }' ${CONF_FILE}`
+            if [[ ! -z "${EXECUTORS_CORES}" ]]
+            then
+                 SPARK_CLUSTER_OPTIONS="${SPARK_CLUSTER_OPTIONS} --executor-cores ${EXECUTORS_CORES}"
+            fi
+
+            EXECUTORS_MEMORY=`awk '{ if( $1 == "spark.executor.memory:" ){ print $2 } }' ${CONF_FILE}`
+            if [[ ! -z "${EXECUTORS_MEMORY}" ]]
+            then
+                 SPARK_CLUSTER_OPTIONS="${SPARK_CLUSTER_OPTIONS} --executor-memory ${EXECUTORS_MEMORY}"
+            fi
+
+            EXECUTORS_INSTANCES=`awk '{ if( $1 == "spark.executor.instances:" ){ print $2 } }' ${CONF_FILE}`
+            if [[ ! -z "${EXECUTORS_INSTANCES}" ]]
+            then
+                 SPARK_CLUSTER_OPTIONS="${SPARK_CLUSTER_OPTIONS} --num-executors ${EXECUTORS_INSTANCES}"
+            fi
+
+            SPARK_TASK_MAX_FAILURES=`awk '{ if( $1 == "spark.task.maxFailures:" ){ print $2 } }' ${CONF_FILE}`
+            if [[ ! -z "${SPARK_TASK_MAX_FAILURES}" ]]
+            then
+                 SPARK_CLUSTER_OPTIONS="${SPARK_CLUSTER_OPTIONS} --conf spark.task.maxFailures=${SPARK_TASK_MAX_FAILURES}"
+            fi
+
+            PROPERTIES_FILE_PATH=`awk '{ if( $1 == "spark.properties.file.path:" ){ print $2 } }' ${CONF_FILE}`
+            if [[ ! -z "${PROPERTIES_FILE_PATH}" ]]
+            then
+                SPARK_CLUSTER_OPTIONS="${SPARK_CLUSTER_OPTIONS} --properties-file ${PROPERTIES_FILE_PATH}"
+            fi
+
+            SPARK_MONITORING_DRIVER_PORT=`awk '{ if( $1 == "spark.monitoring.driver.port:" ){ print $2 } }' ${CONF_FILE}`
+            if [[ -z "${SPARK_MONITORING_DRIVER_PORT}" ]]
+            then
+                 EXTRA_DRIVER_JAVA_OPTIONS='spark.driver.extraJavaOptions=-Dlog4j.configuration=log4j.properties'
+                 EXTRA_PROCESSOR_JAVA_OPTIONS='spark.executor.extraJavaOptions=-Dlog4j.configuration=log4j.properties'
+            else
+                 EXTRA_DRIVER_JAVA_OPTIONS='spark.driver.extraJavaOptions=-Dlog4j.configuration=log4j.properties -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=0 -Dcom.sun.management.jmxremote.rmi.port=0 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -javaagent:./jmx_prometheus_javaagent-0.10.jar='${SPARK_MONITORING_DRIVER_PORT}':./spark-prometheus.yml'
+                 EXTRA_PROCESSOR_JAVA_OPTIONS='spark.executor.extraJavaOptions=-Dlog4j.configuration=log4j.properties -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=0 -Dcom.sun.management.jmxremote.rmi.port=0 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -javaagent:./jmx_prometheus_javaagent-0.10.jar='${SPARK_MONITORING_DRIVER_PORT}':./spark-prometheus.yml'
+            fi
+
+            CONF_FILE="logisland-configuration.yml"
+
+            ${SPARK_HOME}/bin/spark-submit ${VERBOSE_OPTIONS} ${SPARK_CLUSTER_OPTIONS} \
+            --conf "${EXTRA_DRIVER_JAVA_OPTIONS}" \
+            --conf "${EXTRA_PROCESSOR_JAVA_OPTIONS}" \
+            --class ${app_mainclass} \
+            --jars ${app_classpath} ${engine_jar} \
+             -conf ${CONF_FILE}
+            ;;
+
+          spark-client)
+            SPARK_CLUSTER_OPTIONS="--master ${SPARK_MASTER} --deploy-mode client --conf spark.metrics.namespace=\"${APP_NAME}\""
+
+            if [[ ! -z "${SPARK_APP_NAME}" ]]
+            then
+              SPARK_CLUSTER_OPTIONS="${SPARK_CLUSTER_OPTIONS} --name ${SPARK_APP_NAME}"
+            else
+              SPARK_APP_NAME=`awk '{ if( $1 == "spark.app.name:" ){ print $2 } }' ${CONF_FILE}`
+              if [[ ! -z "${SPARK_APP_NAME}" ]]
+              then
+                SPARK_CLUSTER_OPTIONS="${SPARK_CLUSTER_OPTIONS} --name ${SPARK_APP_NAME}"
+              fi
+            fi
+
+            DRIVER_CORES=`awk '{ if( $1 == "spark.driver.cores:" ){ print $2 } }' ${CONF_FILE}`
+            if [[ ! -z "${DRIVER_CORES}" ]]
+            then
+             SPARK_CLUSTER_OPTIONS="${SPARK_CLUSTER_OPTIONS} --driver-cores ${DRIVER_CORES}"
+            fi
+
+            DRIVER_MEMORY=`awk '{ if( $1 == "spark.driver.memory:" ){ print $2 } }' ${CONF_FILE}`
+            if [[ ! -z "${DRIVER_MEMORY}" ]]
+            then
+             SPARK_CLUSTER_OPTIONS="${SPARK_CLUSTER_OPTIONS} --driver-memory ${DRIVER_MEMORY}"
+            fi
+
+            PROPERTIES_FILE_PATH=`awk '{ if( $1 == "spark.properties.file.path:" ){ print $2 } }' ${CONF_FILE}`
+            if [[ ! -z "${PROPERTIES_FILE_PATH}" ]]
+            then
+                 SPARK_CLUSTER_OPTIONS="${SPARK_CLUSTER_OPTIONS} --properties-file ${PROPERTIES_FILE_PATH}"
+            fi
+
+            SPARK_MONITORING_DRIVER_PORT=`awk '{ if( $1 == "spark.monitoring.driver.port:" ){ print $2 } }' ${CONF_FILE}`
+            if [[ -z "${SPARK_MONITORING_DRIVER_PORT}" ]]
+            then
+                 EXTRA_DRIVER_JAVA_OPTIONS='spark.driver.extraJavaOptions=-Dlog4j.configuration=log4j.properties'
+                 EXTRA_PROCESSOR_JAVA_OPTIONS='spark.executor.extraJavaOptions=-Dlog4j.configuration=log4j.properties'
+            else
+                 EXTRA_DRIVER_JAVA_OPTIONS='spark.driver.extraJavaOptions=-Dlog4j.configuration=log4j.properties -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=0 -Dcom.sun.management.jmxremote.rmi.port=0 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -javaagent:'${CONF_DIR}'/../monitoring/jmx_prometheus_javaagent-0.10.jar='${SPARK_MONITORING_DRIVER_PORT}':'${CONF_DIR}'/../monitoring/spark-prometheus.yml'
+                 EXTRA_PROCESSOR_JAVA_OPTIONS='spark.executor.extraJavaOptions=-Dlog4j.configuration=log4j.properties -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=0 -Dcom.sun.management.jmxremote.rmi.port=0 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -javaagent:./jmx_prometheus_javaagent-0.10.jar='${SPARK_MONITORING_DRIVER_PORT}':./spark-prometheus.yml'
+            fi
+
+            ${SPARK_HOME}/bin/spark-submit ${VERBOSE_OPTIONS} ${SPARK_CLUSTER_OPTIONS} \
+            --conf spark.metrics.conf="${CONF_DIR}/../monitoring/metrics.properties"  \
+            --conf "${EXTRA_DRIVER_JAVA_OPTIONS}" \
+            --conf "${EXTRA_PROCESSOR_JAVA_OPTIONS}" \
+            --class ${app_mainclass} \
+            --jars ${app_classpath} ${engine_jar} \
+            -conf ${CONF_FILE}
+            ;;
+
+          *)
+            echo "Unsupported run mode: ${MODE}"
             ;;
 
           esac
