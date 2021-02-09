c840e356d51f359340a68796d519696fdb6afe0a
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2021-02-09 14:16:52 +0100

    Ignore infinite tests.

--- logisland-tests/webanal-spark-2.4-kafka0.10-streaming-pipeline/src/test/java/com/hurence/logisland/webanalytics/SparkStreamingMultiSourceAndSinkTest.java
+++ logisland-tests/webanal-spark-2.4-kafka0.10-streaming-pipeline/src/test/java/com/hurence/logisland/webanalytics/SparkStreamingMultiSourceAndSinkTest.java
@@ -14,6 +14,7 @@ import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +22,7 @@ import org.springframework.kafka.test.rule.KafkaEmbedded;
 
 import java.io.Serializable;
 
+@Ignore
 public class SparkStreamingMultiSourceAndSinkTest implements Serializable {
 
     private static Logger logger = LoggerFactory.getLogger(SparkStreamingMultiSourceAndSinkTest.class);
@@ -113,170 +115,162 @@ public class SparkStreamingMultiSourceAndSinkTest implements Serializable {
         }
     }
 
-//    /**
-//     *
-//     */
-//    @Test
-//    public void multipleSourceTest() throws Exception {
-//        logger.info("Starting test");
-//        final String topicName = "test-simple";
-//        final String topicName2 = "test-simple2";
-//        sharedKafkaTestResource.getKafkaTestUtils().createTopic(topicName, 1, (short)1);
-//        sharedKafkaTestResource.getKafkaTestUtils().createTopic(topicName2, 1, (short)1);
-//        SparkSession spark = SparkSession
-//                .builder()
-//                .master("local[2]")
-////                .appName("StructuredSessionizationFromKafka")
-//                .getOrCreate();
-//        logger.info("Created SparkSession");
-////
-////    // Subscribe to 1 topic
-//        Dataset<Row> df = spark
-//                .readStream()
-//                .format("kafka")
-//                .option("kafka.bootstrap.servers", sharedKafkaTestResource.getKafkaConnectString())
-//                .option("subscribe", topicName)
-//                .option("startingOffsets", "earliest")//latest by default for stream
-//                .load();
+    /**
+     *
+     */
+    @Test
+    public void multipleSourceTest() throws Exception {
+        logger.info("Starting test");
+        final String topicName = topic1;
+        final String topicName2 = topic2;
+        SparkSession spark = SparkSession
+                .builder()
+                .master("local[2]")
+//                .appName("StructuredSessionizationFromKafka")
+                .getOrCreate();
+        logger.info("Created SparkSession");
 //
-//        Dataset<Row> df2 = spark
-//                .readStream()
-//                .format("kafka")
-//                .option("kafka.bootstrap.servers", sharedKafkaTestResource.getKafkaConnectString())
-//                .option("subscribe", topicName2)
-//                .option("startingOffsets", "earliest")//latest by default for stream
-//                .load();
+//    // Subscribe to 1 topic
+        Dataset<Row> df = spark
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", embeddedKafka.getBrokersAsString())
+                .option("subscribe", topicName)
+                .option("startingOffsets", "earliest")//latest by default for stream
+                .load();
+
+        Dataset<Row> df2 = spark
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", embeddedKafka.getBrokersAsString())
+                .option("subscribe", topicName2)
+                .option("startingOffsets", "earliest")//latest by default for stream
+                .load();
+
+        df = df.union(df2).selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+                .groupBy("key")
+                .count();
+
+        StructType schema = new StructType(
+                new StructField[]{
+                        new StructField("key", DataTypes.StringType, true, null),
+                        new StructField("counter", DataTypes.LongType, true, null)
+                }
+        );
+
+        Dataset<Row> dataset = df.map(new MapFunction<Row, Row>() {
+            @Override
+            public Row call(Row row) throws Exception {
+                KeyValue kv = new KeyValue(row.getString(0), row.getLong(1));
+                logger.info("processing key {} with count {}", kv.key, kv.count);
+                return row;
+            }
+        }, RowEncoder.apply(schema));
+
+        logger.info("Start streaming");
+        // Start running the query that prints the session updates to the console
+        StreamingQuery query = dataset
+                .writeStream()
+                .outputMode("update")
+                .format("console")
+                .option("truncate", false)
+                .start();
+
+        EventsGenerator eventGen = new EventsGenerator("divolte_1");
+        long ts = 0L;
+        while (true) {
+            logger.info("Adding an event in topic");
+            Record event = eventGen.generateEvent(ts, "url");
+            kafkaUtils.addingEventsToTopicPartition(topicName, 0, "fromtopic1", event);
+            kafkaUtils.addingEventsToTopicPartition(topicName2, 0, "fromtopic2", event);
+            logger.info("Waiting 5 sec");
+            long sleep = 5000L;
+            ts += sleep;
+            Thread.sleep(sleep);
+        }
+    }
+
+
+    /**
+     *
+     */
+    @Test
+    public void multipleSourceAndSinkTest() throws Exception {
+        logger.info("Starting test");
+        final String topicName = topic1;
+        final String topicName2 = topic2;
+        SparkSession spark = SparkSession
+                .builder()
+                .master("local[2]")
+//                .appName("StructuredSessionizationFromKafka")
+                .getOrCreate();
+        logger.info("Created SparkSession");
 //
-//        df = df.union(df2).selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-//                .groupBy("key")
-//                .count();
-//
-//        StructType schema = new StructType(
-//                new StructField[]{
-//                        new StructField("key", DataTypes.StringType, true, null),
-//                        new StructField("counter", DataTypes.LongType, true, null)
-//                }
-//        );
-//
-//        Dataset<Row> dataset = df.map(new MapFunction<Row, Row>() {
-//            @Override
-//            public Row call(Row row) throws Exception {
-//                KeyValue kv = new KeyValue(row.getString(0), row.getLong(1));
-//                logger.info("processing key {} with count {}", kv.key, kv.count);
-//                return row;
-//            }
-//        }, RowEncoder.apply(schema));
-//
-//        logger.info("Start streaming");
-//        // Start running the query that prints the session updates to the console
-//        StreamingQuery query = dataset
-//                .writeStream()
-//                .outputMode("update")
-//                .format("console")
-//                .option("truncate", false)
-//                .start();
-//
-//        EventsGenerator eventGen = new EventsGenerator("divolte_1");
-//        long ts = 0L;
-//        while (true) {
-//            logger.info("Adding an event in topic");
-//            Record event = eventGen.generateEvent(ts, "url");
-//            addingEventsToTopicPartition(topicName, 0, "fromtopic1", event);
-//            addingEventsToTopicPartition(topicName2, 0, "fromtopic2", event);
-////            addingEventsToTopicPartition(topicName, 0, "session1", event);
-////            addingEventsToTopicPartition(topicName, 0, "session2", event);
-//            logger.info("Waiting 5 sec");
-//            long sleep = 5000L;
-//            ts += sleep;
-//            Thread.sleep(sleep);
-//        }
-//    }
-//
-//
-//    /**
-//     *
-//     */
-//    @Test
-//    public void multipleSourceAndSinkTest() throws Exception {
-//        logger.info("Starting test");
-//        final String topicName = "test-simple";
-//        final String topicName2 = "test-simple2";
-//        sharedKafkaTestResource.getKafkaTestUtils().createTopic(topicName, 1, (short)1);
-//        sharedKafkaTestResource.getKafkaTestUtils().createTopic(topicName2, 1, (short)1);
-//        SparkSession spark = SparkSession
-//                .builder()
-//                .master("local[2]")
-////                .appName("StructuredSessionizationFromKafka")
-//                .getOrCreate();
-//        logger.info("Created SparkSession");
-////
-////    // Subscribe to 1 topic
-//        Dataset<Row> df = spark
-//                .readStream()
-//                .format("kafka")
-//                .option("kafka.bootstrap.servers", sharedKafkaTestResource.getKafkaConnectString())
-//                .option("subscribe", topicName)
-//                .option("startingOffsets", "earliest")//latest by default for stream
-//                .load();
-//
-//        Dataset<Row> df2 = spark
-//                .readStream()
-//                .format("kafka")
-//                .option("kafka.bootstrap.servers", sharedKafkaTestResource.getKafkaConnectString())
-//                .option("subscribe", topicName2)
-//                .option("startingOffsets", "earliest")//latest by default for stream
-//                .load();
-//
-//        df = df.union(df2).selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-//                .groupBy("key")
-//                .count();
-//
-//        StructType schema = new StructType(
-//                new StructField[]{
-//                        new StructField("key", DataTypes.StringType, true, null),
-//                        new StructField("counter", DataTypes.LongType, true, null)
-//                }
-//        );
-//
-//        Dataset<Row> dataset = df.map(new MapFunction<Row, Row>() {
-//            @Override
-//            public Row call(Row row) throws Exception {
-//                KeyValue kv = new KeyValue(row.getString(0), row.getLong(1));
-//                logger.info("processing key {} with count {}", kv.key, kv.count);
-//                return row;
-//            }
-//        }, RowEncoder.apply(schema));
-//
-//        logger.info("Start streaming");
-//        // Start running the query that prints the session updates to the console
-//        StreamingQuery query = dataset
-//                .writeStream()
-//                .outputMode("update")
-//                .format("console")
-//                .option("truncate", false)
-//                .start();
-//
-//        StreamingQuery query2 = dataset
-//                .writeStream()
-//                .outputMode("update")
-//                .format("console")
-//                .option("truncate", false)
-//                .start();
-//
-//        EventsGenerator eventGen = new EventsGenerator("divolte_1");
-//        long ts = 0L;
-//        while (true) {
-//            logger.info("Adding an event in topic");
-//            Record event = eventGen.generateEvent(ts, "url");
-//            addingEventsToTopicPartition(topicName, 0, "fromtopic1", event);
-//            addingEventsToTopicPartition(topicName2, 0, "fromtopic2", event);
-////            addingEventsToTopicPartition(topicName, 0, "session1", event);
-////            addingEventsToTopicPartition(topicName, 0, "session2", event);
-//            logger.info("Waiting 5 sec");
-//            long sleep = 5000L;
-//            ts += sleep;
-//            Thread.sleep(sleep);
-//        }
-//    }
+//    // Subscribe to 1 topic
+        Dataset<Row> df = spark
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", embeddedKafka.getBrokersAsString())
+                .option("subscribe", topicName)
+                .option("startingOffsets", "earliest")//latest by default for stream
+                .load();
+
+        Dataset<Row> df2 = spark
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", embeddedKafka.getBrokersAsString())
+                .option("subscribe", topicName2)
+                .option("startingOffsets", "earliest")//latest by default for stream
+                .load();
+
+        df = df.union(df2).selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+                .groupBy("key")
+                .count();
+
+        StructType schema = new StructType(
+                new StructField[]{
+                        new StructField("key", DataTypes.StringType, true, null),
+                        new StructField("counter", DataTypes.LongType, true, null)
+                }
+        );
+
+        Dataset<Row> dataset = df.map(new MapFunction<Row, Row>() {
+            @Override
+            public Row call(Row row) throws Exception {
+                KeyValue kv = new KeyValue(row.getString(0), row.getLong(1));
+                logger.info("processing key {} with count {}", kv.key, kv.count);
+                return row;
+            }
+        }, RowEncoder.apply(schema));
+
+        logger.info("Start streaming");
+        // Start running the query that prints the session updates to the console
+        StreamingQuery query = dataset
+                .writeStream()
+                .outputMode("update")
+                .format("console")
+                .option("truncate", false)
+                .start();
+
+        StreamingQuery query2 = dataset
+                .writeStream()
+                .outputMode("update")
+                .format("console")
+                .option("truncate", false)
+                .start();
+
+        EventsGenerator eventGen = new EventsGenerator("divolte_1");
+        long ts = 0L;
+        while (true) {
+            logger.info("Adding an event in topic");
+            Record event = eventGen.generateEvent(ts, "url");
+            kafkaUtils.addingEventsToTopicPartition(topicName, 0, "fromtopic1", event);
+            kafkaUtils.addingEventsToTopicPartition(topicName2, 0, "fromtopic2", event);
+            logger.info("Waiting 5 sec");
+            long sleep = 5000L;
+            ts += sleep;
+            Thread.sleep(sleep);
+        }
+    }
 
 }
