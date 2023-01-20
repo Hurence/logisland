f40e756f32cd60de8108faab17306e2a5af6710d
Cyril <cyril.tissot@hurence.com>
2023-01-20 14:40:53 +0100

    Added filters to reduce logs.

--- logisland-components/logisland-processors/logisland-processor-common/src/main/java/com/hurence/logisland/processor/DebugStream.java
+++ logisland-components/logisland-processors/logisland-processor-common/src/main/java/com/hurence/logisland/processor/DebugStream.java
@@ -19,18 +19,23 @@ import com.hurence.logisland.annotation.documentation.*;
 import com.hurence.logisland.component.AllowableValue;
 import com.hurence.logisland.component.InitializationException;
 import com.hurence.logisland.component.PropertyDescriptor;
+import com.hurence.logisland.record.Field;
 import com.hurence.logisland.record.Record;
+import com.hurence.logisland.record.StandardRecord;
 import com.hurence.logisland.serializer.*;
 import com.hurence.logisland.validator.StandardValidators;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Category(ComponentCategory.UTILS)
 @Tags({"record", "debug"})
@@ -69,19 +74,35 @@ public class DebugStream extends AbstractProcessor {
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
             .build();
 
+    static final PropertyDescriptor DEBUG_FILTER = new PropertyDescriptor.Builder()
+            .name("debug.filter")
+            .description("A filter that will look for the provided key with the specified value: <key>:<regexp>. Eg")
+            .required(false)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
 
-
+    static final PropertyDescriptor DEBUG_FILTER_SELECTION = new PropertyDescriptor.Builder()
+            .name("debug.selection")
+            .description("A comma separated fieldnames to narrow the number of fields printed out.")
+            .required(false)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
 
     @Override
     public final List<PropertyDescriptor> getSupportedPropertyDescriptors() {
         final List<PropertyDescriptor> descriptors = new ArrayList<>();
         descriptors.add(SERIALIZER);
         descriptors.add(RECORD_SCHEMA);
+        descriptors.add(DEBUG_FILTER);
+        descriptors.add(DEBUG_FILTER_SELECTION);
         return Collections.unmodifiableList(descriptors);
     }
 
     private volatile MemoryMXBean memBean;
     private volatile RecordSerializer serializer;
+    private volatile String key;
+    private volatile Pattern filter;
+    private volatile Collection<String> selection;
 
     @Override
     public void init(ProcessContext context)  throws InitializationException {
@@ -101,6 +122,38 @@ public class DebugStream extends AbstractProcessor {
                 serializer = new StringSerializer();
             }
         }
+
+        /*
+         * Supports filtering to avoid all records to be fully dumped.
+         * For eg.
+         *     debug.filter: Userid:8822736715780
+         *     debug.selection: Class,codeProduct
+         * will dump only records with field 'Userid=8822736715780' and will only print out fields 'Class,codeProduct'.
+         */
+        if (context.getPropertyValue(DEBUG_FILTER).isSet()) {
+            final String filter = context.getPropertyValue(DEBUG_FILTER).asString();
+            try {
+                final int index = filter.indexOf(':');
+                this.key = filter.substring(0, index);
+                final String regexp = filter.substring(index+1);
+                this.filter = Pattern.compile(regexp);
+
+                if (context.getPropertyValue(DEBUG_FILTER_SELECTION).isSet()) {
+                    this.selection = Arrays.stream(context.getPropertyValue(DEBUG_FILTER_SELECTION).asString()
+                                           .split(","))
+                                           .map(String::trim)
+                                           .collect(Collectors.toList());
+                }
+
+                if ( getLogger().isTraceEnabled() ) {
+                    getLogger().trace("Successfully setup filter for DebugStream with => key='" + this.key + "' and regexp='" + regexp + "'" +
+                                      (this.selection == null ? "" : ", selection=" + this.selection));
+                }
+            }
+            catch (final Exception e) {
+                throw new RuntimeException("Invalid filter for '"+filter+"'. Was expecting 'key:<regexp>'", e);
+            }
+        }
     }
 
     @Override
@@ -111,9 +164,33 @@ public class DebugStream extends AbstractProcessor {
         if (collection.size() != 0) {
             //Do not use serialization ! It is pointless as at this point the object is already deserialized into a Record !
             //Moreover trying to serialize the record may fail for a lot of reason (if record contains some objects without appropriate bean pattern.
-            collection.forEach(record -> {
-                getLogger().info(record.toString(1));
-            });
+            for(Record record: collection) {
+                boolean log = true; // log by default.
+                if ( this.filter!=null ) {
+                    // If a filter is defined check if the field to check is present in the record.
+                    final Field field = record.getField(this.key);
+                    if ( field != null ) {
+                        final String value = field.asString();
+                        if ( value!=null ) {
+                            // Check if the fields matches the regexp.
+                            log = this.filter.matcher(value).find();
+                            if ( log ) {
+                                if ( this.selection!=null ) {
+                                    // Prints out only select fields.
+                                    final Map<String, Field> fields = record.getAllFields().stream()
+                                                                            .filter(f -> this.selection.contains(f.getName()))
+                                                                            .collect(Collectors.toMap(Field::getName,Function.identity()));
+                                    record = new StandardRecord();
+                                    record.addFields(fields);
+                                }
+                            }
+                        }
+                    }
+                }
+                if ( log ) {
+                    getLogger().info(record.toString(1));
+                }
+            }
         }
 
         return collection;
